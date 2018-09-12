b2817182eb2ec16095544dbf5d62b892d11e8d40
Gregor von Laszewski <laszewski@gmail.com>
2018-09-12 05:07:58 -0400

    Update vagrant.py

--- vagrant.py
+++ vagrant.py
@@ -11,8 +11,14 @@ Usage:
 Options:
   -h --help     Show this screen.
   --vm_list=<list_of_vms>  List of VMs separated by commas ex: node-1,node-2
+  
+Description:
+   put a description here
+   
+Example:
+   put an example here
 """
-
+from __future__ import print_function
 import fileinput
 import re
 import subprocess
@@ -26,71 +32,82 @@ def main():
 
 def process_arguments(arguments):
     if arguments.get("vagrant"):
+        provider = vagrant()
         if arguments.get("create") & arguments.get("--vms"):
-            generate_vagrant_file(arguments.get("<vm_number>"))
+            provider.generate_file(arguments.get("<vm_number>"))
         elif arguments.get("start"):
             if arguments.get("--vm_list"):
                 for node_name in arguments.get("--vm_list").split(','):
-                    start_vagrant_vm(node_name)
+                    provider.start_vm(node_name)
             else:
                 start_all_vagrant_vms()
         elif arguments.get("stop"):
             if arguments.get("--vm_list"):
                 for node_name in arguments.get("--vm_list").split(','):
-                    stop_vagrant_vm(node_name)
+                    provider.stop_vm(node_name)
             else:
                 stop_all_vagrant_vms()
         elif arguments.get("destroy"):
             if arguments.get("--vm_list"):
                 for node_name in arguments.get("--vm_list").split(','):
-                    destroy_vagrant_vm(node_name)
+                    provider.destroy_vm(node_name)
             else:
                 destroy_all_vagrant_vms()
         elif arguments.get("status"):
             if arguments.get("--vm_list"):
                 for node_name in arguments.get("--vm_list").split(','):
-                    status_vagrant_vm(node_name)
+                    provider.status_vm(node_name)
             else:
-                status_all_vagrant_vms()
+                provider.status_all_vms()
+                
+class vagrant(object):
+  
+  def __init__():
+    self.workspace = "./vagrant_workspace"
+    
+  def execute(command):
+    subprocess.run(command, 
+                   cwd=self.workspace, 
+                   check=True, 
+                   shell=True)
+  
+  def status_all_vms():
+     self.execute("vagrant status")
 
 
-def status_all_vagrant_vms():
-    subprocess.run("vagrant status", cwd="./vagrant_workspace", check=True, shell=True)
+  def status_vm(name):
+      execute("vagrant status " + str(name))
 
 
-def status_vagrant_vm(vm_name):
-    subprocess.run("vagrant status " + str(vm_name), cwd="./vagrant_workspace", check=True, shell=True)
+  def start_all_vms():
+      self.execute("vagrant up")
 
 
-def start_all_vagrant_vms():
-    subprocess.run("vagrant up", cwd="./vagrant_workspace", check=True, shell=True)
+  def start_vm(name):
+      self.execute("vagrant up " + str(name))
 
 
-def start_vagrant_vm(vm_name):
-    subprocess.run("vagrant up " + str(vm_name), cwd="./vagrant_workspace", check=True, shell=True)
+  def stop_all_vms():
+      self.execute("vagrant halt")
 
 
-def stop_all_vagrant_vms():
-    subprocess.run("vagrant halt", cwd="./vagrant_workspace", check=True, shell=True)
+  def stop_vagrant_vm(name):
+      self.execute("vagrant halt " + str(name))
 
 
-def stop_vagrant_vm(vm_name):
-    subprocess.run("vagrant halt " + str(vm_name), cwd="./vagrant_workspace", check=True, shell=True)
+  def destroy_all_vms():
+      self.execute("vagrant destroy")
 
 
-def destroy_all_vagrant_vms():
-    subprocess.run("vagrant destroy", cwd="./vagrant_workspace", check=True, shell=True)
+  def destroy_vm(name):
+      self.execute("vagrant destroy " + str(name))
 
 
-def destroy_vagrant_vm(vm_name):
-    subprocess.run("vagrant destroy " + str(vm_name), cwd="./vagrant_workspace", check=True, shell=True)
-
-
-def generate_vagrant_file(number_of_nodes):
-    replacement_string = "NUMBER_OF_NODES = " + str(number_of_nodes)
-    for line in fileinput.FileInput("vagrant_workspace/Vagrantfile", inplace=True):
-        line = re.sub("NUMBER_OF_NODES.*(\d+)$", replacement_string, line.rstrip())
-        print(line)
+  def generate_vagrantfile(number_of_nodes):
+      replacement_string = "NUMBER_OF_NODES = " + str(number_of_nodes)
+      for line in fileinput.FileInput(os.path.join(self.workspace, "Vagrantfile"), inplace=True):
+          line = re.sub("NUMBER_OF_NODES.*(\d+)$", replacement_string, line.rstrip())
+          print(line)
 
 
 if __name__ == "__main__":

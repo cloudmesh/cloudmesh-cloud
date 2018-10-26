f1173e14b198534bfa44d03a1eb75dcd97e1ca6b
Yu Luo <luoyu357@gmail.com>
2018-10-26 02:09:52 -0400

    1. update some abstract class, need other users' feedbacks. 2. update the aws controller and command class. 3. copy yaml file to aws folder, some updates on it. 4. update mongodb access code

--- cm4/abstractclass/CloudManagerABC.py
+++ cm4/abstractclass/CloudManagerABC.py
@@ -3,27 +3,61 @@ import abc
 
 class CloudManagerABC(metaclass=abc.ABCMeta):
 
+    # please update the abstract class
+    # this abstract class used for any cloud instance
+
     @abc.abstractmethod
     def start(self):
+        """
+        start node
+        """
         pass
 
     @abc.abstractmethod
     def stop(self):
+        """
+        stop node
+        """
         pass
 
     @abc.abstractmethod
     def info(self):
-        pass
-
-    @abc.abstractmethod
-    def delete(self):
+        """
+        get all information about one node
+        """
         pass
 
     @abc.abstractmethod
     def suspend(self):
+        """
+        suspend one node
+        """
         pass
-
 
     @abc.abstractmethod
     def ls(self):
+        """
+        list all nodes id
+        """
+        pass
+
+    @abc.abstractmethod
+    def resume(self):
+        """
+        resume one node
+        """
+        pass
+
+    @abc.abstractmethod
+    def destroy(self):
+        """
+        delete one node
+        """
+        pass
+
+    @abc.abstractmethod
+    def create(self):
+        """
+        create one node
+        """
         pass

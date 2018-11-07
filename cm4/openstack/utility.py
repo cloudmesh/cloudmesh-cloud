c0aa751489dcb1f972529772a1a681bcec8e16b5
Chun-Sheng Wu <kimballXD@gmail.com>
2018-11-07 17:50:42 -0500

    delete files of no use

--- cm4/openstack/utility.py
+++
@@ -1,5 +0,0 @@
-from cm4.abstractclass.CloudManagerABC import CloudManagerABC
-from libcloud.compute.types import Provider
-from libcloud.compute.providers import get_driver
-import os
-from config import Config

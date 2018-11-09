0ae3a442ba076cb4dd1cdadfc233bf2a8a9182d8
Yu Luo <luoyu357@gmail.com>
2018-11-09 11:41:09 -0500

    debug

--- cm4/vm/demo.py
+++ cm4/vm/demo.py
@@ -2,11 +2,12 @@ from cm4.vm.Vm import Vm
 
 def main():
 
-    #test = Vm('aws')
+    test = Vm('aws')
     #test.start('i-0fad7e92ffea8b345')
     #test.stop('i-0fad7e92ffea8b345')
+    print(test.list())
     #print(test.status('i-0fad7e92ffea8b345'))
-    #test.mongo.close_client()
+    test.mongo.close_client()
 
     #test = Vm('azure')
     #print(test.list()[0].id)

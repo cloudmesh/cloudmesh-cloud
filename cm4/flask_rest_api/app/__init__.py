7d537fa5567b0c018efa0354d0ba94ec771f4844
swsachith <swsachith@gmail.com>
2018-11-12 10:28:26 -0500

    Reading the mongo configs from the cm4 Configuration yaml file

--- cm4/flask_rest_api/app/__init__.py
+++ cm4/flask_rest_api/app/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask_pymongo import PyMongo
 
-from cm4.flask_rest_api.rest_config import Config
+from cm4.flask_rest_api.rest_config import RestConfig
 
 app = Flask(__name__)
-app.config.from_object(Config)
+app.config.from_object(RestConfig)
 
 mongo = PyMongo(app)
 cloud = mongo.db.cloud
--- cm4/flask_rest_api/rest_config.py
+++ cm4/flask_rest_api/rest_config.py
@@ -1,7 +1,11 @@
-class Config(object):
-    MONGO_DBNAME = "cloudmesh"
-    MONGO_HOST = "127.0.0.1"
-    MONGO_PORT = "27017"
-    MONGO_USERNAME = "admin"
-    MONGO_PASSWORD = "admin"
+from cm4.configuration.config import Config
+
+
+class RestConfig(object):
+    config = Config()
+    MONGO_DBNAME = config.get("data.mongo.MONGO_DBNAME")
+    MONGO_HOST = config.get("data.mongo.MONGO_HOST")
+    MONGO_PORT = config.get("data.mongo.MONGO_PORT")
+    MONGO_USERNAME = config.get("data.mongo.MONGO_USERNAME")
+    MONGO_PASSWORD = config.get("data.mongo.MONGO_PASSWORD")
     MONGO_URI = "mongodb://" + MONGO_USERNAME + ":" + MONGO_PASSWORD + "@" + MONGO_HOST + ":" + MONGO_PORT + "/" + MONGO_DBNAME

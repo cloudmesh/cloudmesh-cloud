2efc3caed4139b7c5af469142a7596da85eb03c5
Rui Li <ruili@ruidembp.attlocal.net>
2018-11-16 23:58:32 -0500

    init working on flavors for all vms, add test script to get flavor/size objects from cloud

--- cm4/openstack/test/demo_refactors.py
+++ cm4/openstack/test/demo_refactors.py
@@ -10,6 +10,8 @@ from cm4.openstack.OpenstackCM import OpenstackCM
 from cm4.openstack.OpenstackRefactor import OpenstackRefactor
 from time import sleep
 import datetime
+import pprint as pp
+
 
 # testcode
 # need extra waiting time so server can finish update the node states
@@ -19,6 +21,14 @@ import datetime
 def main():
     d = OpenstackCM('chameleon')
     r = OpenstackRefactor(d)
+
+    '''
+    sizes = r.list_sizes()
+    images = r.list_images()
+    for im in images:
+        pp.pprint(im.__dict__)
+    return
+    '''
 
     ## create and auto start
     print("call d.create() function")

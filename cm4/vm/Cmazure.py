bea146900ce781aeddaea030c223f3d577337957
Dave DeMeulenaere <davidmdem@gmail.com>
2018-11-19 22:03:01 -0500

    pep8 updates for vm classes
    no functionality changes

--- cm4/vm/Cmazure.py
+++ cm4/vm/Cmazure.py
@@ -1,6 +1,5 @@
 from libcloud.compute.types import Provider
 from libcloud.compute.providers import get_driver
-from libcloud.compute.base import NodeAuthSSHKey
 from cm4.vm.Cloud import Cloud
 
 
@@ -11,23 +10,10 @@ class Cmazure(Cloud):
         os_config = config.get('cloud.%s' % cloud)
         default = os_config.get('default')
         credentials = os_config.get('credentials')
-        self.driver = cls(tenant_id=credentials['AZURE_TENANT_ID'],
-                     subscription_id=credentials['AZURE_SUBSCRIPTION_ID'],
-                     key=credentials['AZURE_APPLICATION_ID'],
-                     secret=credentials['AZURE_SECRET_KEY'],
-                     region=default['region'])
-        '''
-        size = [s for s in driver.list_sizes() if s.id == default['size']][0]
-        image = [i for i in driver.list_images() if i.id == default['image']][0]
-        self.setting = dict(size=size, image=image, auth=NodeAuthSSHKey(default['AZURE_MANAGEMENT_CERT_PATH']),
-                            ex_use_managed_disks=True, ex_resource_group=default['resource_group'],
-                            ex_storage_account=default['storage_account'],
-                            ex_network=default['network']
-                            )
-        '''
-
-
-    '''
-    def get_new_node_setting(self):
-        return self.setting
-    '''
+        self.driver = cls(
+            tenant_id=credentials['AZURE_TENANT_ID'],
+            subscription_id=credentials['AZURE_SUBSCRIPTION_ID'],
+            key=credentials['AZURE_APPLICATION_ID'],
+            secret=credentials['AZURE_SECRET_KEY'],
+            region=default['region']
+        )
--- cm4/vm/Cmopenstack.py
+++ cm4/vm/Cmopenstack.py
@@ -8,25 +8,13 @@ class Cmopenstack(Cloud):
     def __init__(self, config, cloud):
         cls = get_driver(Provider.OPENSTACK)
         os_config = config.get('cloud.%s' % cloud)
-        default = os_config.get('default')
         credentials = os_config.get('credentials')
 
-        self.driver = cls(credentials['OS_USERNAME'],
-                     credentials['OS_PASSWORD'] or os.environ['OS_PASSWORD'],
-                     ex_tenant_name=credentials['OS_TENANT_NAME'],
-                     ex_force_auth_url=credentials['OS_AUTH_URL'],
-                     ex_force_auth_version=credentials['OS_VERSION'],
-                     ex_force_service_region=credentials['OS_REGION_NAME']
-                    )
-
-        '''
-        size = [s for s in driver.list_sizes() if s.id == default['flavor']][0]
-        image = [i for i in driver.list_images() if i.id == default['image']][0]
-        self.setting = dict(size=size, image=image)
-        '''
-
-
-    '''
-    def get_new_node_setting(self):
-        return self.setting
-    '''
+        self.driver = cls(
+            credentials['OS_USERNAME'],
+            credentials['OS_PASSWORD'] or os.environ['OS_PASSWORD'],
+            ex_tenant_name=credentials['OS_TENANT_NAME'],
+            ex_force_auth_url=credentials['OS_AUTH_URL'],
+            ex_force_auth_version=credentials['OS_VERSION'],
+            ex_force_service_region=credentials['OS_REGION_NAME']
+        )

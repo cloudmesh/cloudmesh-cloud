ce37889dcbfe7c397e4b4b1db85babcac73b2df5
Yu Luo <luoyu357@gmail.com>
2018-11-29 18:08:41 -0500

    update the Cmaws file

--- cm4/vm/Cmaws.py
+++ cm4/vm/Cmaws.py
@@ -14,7 +14,7 @@ class Cmaws(Cloud):
         os_config = config.get('cloud.%s' % kind)
         default = os_config.get('default')
         credentials = os_config.get('credentials')
-        self.driver = cls(
+        self.driver = CmAWSDriver(
             credentials['EC2_ACCESS_ID'],
             credentials['EC2_SECRET_KEY'],
             region=default['region']
@@ -24,28 +24,21 @@ class Cmaws(Cloud):
 
 class CmAWSDriver(EC2NodeDriver, NodeDriver):
 
-    def __init__(self, id, key, region):
+    def __init__(self, id, key, region, **kwargs):
         config = Config()
         self.default = config.get("cloud.aws.default")
-
-        super().__init__(key=id, secret=key, region=region)
-
-    def start(self, node):
-        self.ex_start_node(node)
+        super().__init__(key=id, secret=key, region=region, **kwargs)
 
 
-    def stop(self, node):
-        self.ex_stop_node(node)
+    def ex_stop_node(self, node, deallocate):
+        super().ex_stop_node(node)
 
-
-    def destroy_node(self, node):
-        self.destroy_node(node)
 
 
     def create_node(self, name):
         size = [s for s in self.list_sizes() if s.id == self.default['size']][0]
         image = [i for i in self.list_images() if i.id == self.default['image']][0]
-        new_vm = self.create_node(name=name, image=image, size=size, ex_keyname=self.default['EC2_PRIVATE_KEY_FILE_NAME'],
+        new_vm = super().create_node(name=name, image=image, size=size, ex_keyname=self.default['EC2_PRIVATE_KEY_FILE_NAME'],
                              ex_securitygroup=self.default['EC2_SECURITY_GROUP'])
 
         return new_vm

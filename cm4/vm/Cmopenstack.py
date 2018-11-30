e7f55eda936c7f1659ea66b028b2b657018a78c5
Rui Li <ruili@ruidembp.attlocal.net>
2018-11-29 21:17:02 -0500

    clean up for cmopenstack

--- cm4/vm/Cmopenstack.py
+++ cm4/vm/Cmopenstack.py
@@ -2,21 +2,17 @@ from libcloud.compute.types import Provider
 from libcloud.compute.providers import get_driver
 from cm4.openstack.OpenstackCM import OpenstackCM
 from cm4.vm.Cloud import Cloud
+from cm4.configuration.config import Config
+from libcloud.compute.drivers.openstack import OpenStackNodeDriver
+from libcloud.compute.base import NodeDriver
 import os
+
+
 
 class Cmopenstack(Cloud):
 
     def __init__(self, config, cloud):
-#        cls = get_driver(Provider.OPENSTACK)
-#        os_config = config.get('cloud.%s' % cloud)
-#        credentials = os_config.get('credentials')
-        self.driver = OpenstackCM(cloud)
+        self.driver = OpenstackCM(cloud)   # cloud is chameleon
         
-#        cls(
-#            credentials['OS_USERNAME'],
-#            credentials['OS_PASSWORD'] or os.environ['OS_PASSWORD'],
-#            ex_tenant_name=credentials['OS_TENANT_NAME'],
-#            ex_force_auth_url=credentials['OS_AUTH_URL'],
-#            ex_force_auth_version=credentials['OS_VERSION'],
-#            ex_force_service_region=credentials['OS_REGION_NAME']
-#        )
+
+

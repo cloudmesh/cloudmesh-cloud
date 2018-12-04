f674c7d26e66e21c918860bcd679143a0de8898a
Yu Luo <luoyu357@gmail.com>
2018-12-04 16:22:13 -0500

    up-to-date

--- cm4/flask_rest_api/rest_config.py
+++ cm4/flask_rest_api/rest_config.py
@@ -2,10 +2,10 @@ from cm4.configuration.config import Config
 
 
 class RestConfig(object):
-    config = Config().get('data.mongo')
-    MONGO_DBNAME = config.get ("data.mongo.MONGO_DBNAME")
-    MONGO_HOST = config.get ("data.mongo.MONGO_HOST")
-    MONGO_PORT = config.get ("data.mongo.MONGO_PORT")
-    MONGO_USERNAME = config.get ("data.mongo.MONGO_USERNAME")
-    MONGO_PASSWORD = config.get ("data.mongo.MONGO_PASSWORD")
-    MONGO_URI = "mongodb://" + MONGO_USERNAME + ":" + MONGO_PASSWORD + "@" + MONGO_HOST + ":" + MONGO_PORT + "/" + MONGO_DBNAME
+    config = Config()
+    MONGO_DBNAME = config.get("data.mongo.MONGO_DBNAME")
+    MONGO_HOST = config.get("data.mongo.MONGO_HOST")
+    MONGO_PORT = config.get("data.mongo.MONGO_PORT")
+    MONGO_USERNAME = config.get("data.mongo.MONGO_USERNAME")
+    MONGO_PASSWORD = config.get("data.mongo.MONGO_PASSWORD")
+    MONGO_URI = "mongodb://" + MONGO_USERNAME + ":" + MONGO_PASSWORD + "@" + MONGO_HOST + ":" + MONGO_PORT + "/" + MONGO_DBNAME+ MONGO_USERNAME + ":" + MONGO_PASSWORD + "@" + MONGO_HOST + ":" + MONGO_PORT + "/" + MONGO_DBNAME

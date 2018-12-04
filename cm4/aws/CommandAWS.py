9802e4c02a89da6cad03c04a7341fb1faa2e3277
Yu Luo <luoyu357@gmail.com>
2018-12-04 15:13:13 -0500

    reformat the code

--- cm4/aws/CommandAWS.py
+++ cm4/aws/CommandAWS.py
@@ -13,15 +13,15 @@ class CommandAWS(object):
                              password=config.get('data.mongo.MONGO_PASSWORD'),
                              port=config.get('data.mongo.MONGO_PORT'))
 
-    def find_node_DNS(self, vm_name):
+    def find_node_dns(self, vm_name):
         """
         based on AWS design, we need to get the DNS of the required nodes
-        :param name_id: node is
+        :param vm_name: the name of vm
         :return: the DNS
         """
 
-        DNS = self.mongo.find_document('cloud', 'name', vm_name).get('extra').get('dns_name')
-        return DNS
+        dns = self.mongo.find_document('cloud', 'name', vm_name).get('extra').get('dns_name')
+        return dns
 
     def read_script(self, script):
         """
@@ -41,10 +41,10 @@ class CommandAWS(object):
         :param vm_name: the vm name
         :return: the result of command
         """
-        username = 'ubuntu@'+self.find_node_DNS(vm_name)
+        username = 'ubuntu@'+self.find_node_dns(vm_name)
         job_id = self.job_start_update_mongo('Null', command, vm_name)
         self.update_instance_job_status(vm_name, job_id)
-        temp = subprocess.check_output(['ssh', '-i', self.private_key_file, username, command]).decode ("utf-8")
+        temp = subprocess.check_output(['ssh', '-i', self.private_key_file, username, command]).decode("utf-8")
         self.job_end_update_mongo(job_id, temp)
         self.update_instance_job_status(vm_name, job_id)
 
@@ -57,12 +57,12 @@ class CommandAWS(object):
         :param vm_name: the vm name
         :return: the result of script
         """
-        username = 'ubuntu@' + self.find_node_DNS(vm_name)
+        username = 'ubuntu@' + self.find_node_dns(vm_name)
         content = self.read_script(script)
 
         job_id = self.job_start_update_mongo(content, 'Null', vm_name)
         self.update_instance_job_status(vm_name, job_id)
-        temp = subprocess.check_output(['ssh', '-i', self.private_key_file, username, content]).decode ("utf-8")
+        temp = subprocess.check_output(['ssh', '-i', self.private_key_file, username, content]).decode("utf-8")
         self.job_end_update_mongo(job_id, temp)
         self.update_instance_job_status(vm_name, job_id)
 
@@ -81,37 +81,38 @@ class CommandAWS(object):
         return self.mongo.insert_job_collection(job)
 
 
-    def job_end_update_mongo(self, id, output):
+    def job_end_update_mongo(self, document_id, output):
         """
         jod is done, update the information into job collection in MongoDB, status is done
         :param id: the job document id
         :param output: the result
         :return: True/False
         """
-        return self.mongo.update_document('job', '_id', id, dict(status='done', output=output))
+        return self.mongo.update_document('job', '_id', document_id, dict(status='done', output=output))
 
 
     def update_instance_job_status(self, vm_name, job_id):
         """
         for each node, it has its own status. when job is running, it will update its currentJob. When the jon id one,
         currentJob is Null, and history will be update
-        :param vm: node id
+        :param vm_name: the name of vm
         :param job_id: jod id
         """
         status = self.mongo.find_document('status', 'name', vm_name)
         if status:
-            self.mongo.insert_status_collection(vm_name, 'processing', job_id, [])
+            self.mongo.insert_status_collection(self.mongo.status_document(vm_name, 'processing', job_id, []))
         else:
             history = status['history']
             if status['currentJob'] == job_id:
                 history.append(job_id)
-                self.database.update_document('status', vm_name, dict (status='No Job', currentJob='Null', history=history))
+                self.mongo.update_document('status', vm_name, dict(status='No Job', currentJob='Null',
+                                                                      history=history))
             else:
-                self.database.update_document('status', vm_name, dict (status='processing', currentJob=job_id))
+                self.mongo.update_document('status', vm_name, dict(status='processing', currentJob=job_id))
 
 
     def disconnect(self):
         """
         disconnect from mongodb
         """
-        self.database.close_client ()
+        self.mongo.close_client()

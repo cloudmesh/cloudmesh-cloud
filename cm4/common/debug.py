d8af8554e7b5a9f1c329e74de037e977e752e627
Gregor von Laszewski <laszewski@gmail.com>
2018-12-05 02:08:09 -0500

    more cleanup

--- cm4/common/debug.py
+++ cm4/common/debug.py
@@ -4,8 +4,9 @@ import inspect
 def HEADING(name):
     print()
     print("#", 79 * "-")
-    print ("#", name)
-    print ("#", 79 * "-")
+    print("#", name)
+    print("#", 79 * "-")
     print()
+
 
 myself = lambda: inspect.stack()[1][3]

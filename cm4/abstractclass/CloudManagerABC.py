d9b5586abc60605bb12d114cafcdb3fe24c8a0ec
Gregor von Laszewski <laszewski@gmail.com>
2018-12-05 09:50:42 -0500

    update ABC cloud class

--- cm4/abstractclass/CloudManagerABC.py
+++ cm4/abstractclass/CloudManagerABC.py
@@ -3,30 +3,42 @@ import abc
 
 class CloudManagerABC(metaclass=abc.ABCMeta):
     @abc.abstractmethod
-    def start(self, node_id):
+    def start(self, name):
         """
-        start node
+        start a node
+
+        :param name: the unique node name
+        :return:  The dict representing the node
         """
         pass
 
     @abc.abstractmethod
-    def stop(self, node_id):
+    def stop(self, name):
         """
-        stop node
+        stops the node with the given name
+
+        :param name:
+        :return: The dict representing the node including updated status
         """
         pass
 
     @abc.abstractmethod
-    def info(self, node_id):
+    def info(self, name):
         """
-        get all information about one node
+        gets the information of a node with a given name
+
+        :param name:
+        :return: The dict representing the node including updated status
         """
         pass
 
     @abc.abstractmethod
-    def suspend(self, node_id):
+    def suspend(self, name):
         """
-        suspend one node
+        suspends the node with the given name
+
+        :param name: the name of the node
+        :return: The dict representing the node
         """
         pass
 
@@ -34,25 +46,43 @@ class CloudManagerABC(metaclass=abc.ABCMeta):
     def ls(self):
         """
         list all nodes id
+
+        :return: an array of dicts representing the nodes
         """
         pass
 
     @abc.abstractmethod
-    def resume(self, node_id):
+    def resume(self, name):
         """
-        resume one node
+        resume the named node
+
+        :param name: the name of the node
+        :return: the dict of the node
         """
         pass
 
     @abc.abstractmethod
-    def destroy(self, node_id):
+    def destroy(self, name):
         """
-        delete one node
+        Destroys the node
+        :param name: the name of the node
+        :return: the dict of the node
         """
         pass
 
     @abc.abstractmethod
-    def create(self, name, image=None, size=None, timeout=300, **kwargs):
+    def create(self, name, image=None, size=None, timeout=360, **kwargs):
+        """
+        creates a named node
+
+        :param name: the name of the node
+        :param image: the image used
+        :param size: the size of the image
+        :param timeout: a timeout in seconds that is invoked in case the image does not boot.
+               The default is set to 3 minutes.
+        :param kwargs: additional arguments passed along at time of boot
+        :return:
+        """
         """
         create one node
         """
@@ -60,7 +90,11 @@ class CloudManagerABC(metaclass=abc.ABCMeta):
 
     def rename(self, name, new_name):
         """
-        renames the node
+        rename a node
+
+        :param name: the current name
+        :param new_name: the new name
+        :return: the dict with the new name
         """
         pass
 

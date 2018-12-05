57cd2b02c8e2e2ae5928c74017709416eb6bda2d
Gregor von Laszewski <laszewski@gmail.com>
2018-12-05 13:18:35 -0500

    tracing down the error in config

--- setup.py
+++ setup.py
@@ -35,31 +35,30 @@ if sys.version_info > (3, 7, 0):
 
 command = None
 this_platform = platform.system().lower()
-if this_platform in ['darwin']:
-    command = "easy_install readline"
-elif this_platform in ['windows']:
-    command = "pip install pyreadline"
-
-if command is not None:
-    print("Install readline")
-    os.system(command)
+#if this_platform in ['darwin']:
+#    command = "easy_install readline"
+#elif this_platform in ['windows']:
+#    command = "pip install pyreadline"
+#if command is not None:
+#    print("Install readline")
+#    os.system(command)
 
 requiers = """
-apache-libcloud==2.3.0
-certifi==2018.10.15
-chardet==3.0.4
-colorama==0.4.0
-config==0.4.0
-coverage==4.5.1
-docopt==0.6.2
-idna==2.7
-oyaml==0.7
-pymongo==3.7.2
-python-hostlist==1.18
-PyYAML==3.13
-requests==2.20.0
-termcolor==1.1.0
-urllib3==1.24
+apache-libcloud
+certifi
+chardet
+colorama
+config
+coverage
+docopt
+idna
+oyaml
+pymongo
+python-hostlist
+PyYAML
+requests
+termcolor
+urllib3
 six
 """.split("\n")
 
@@ -87,7 +86,7 @@ setup(
     name="cloudmesh_cm4",
     description="cloudmesh_cm4 - A heterogeneous multi cloud command "
                 "client and shell",
-    long_description=read('README-aws.md'),
+    long_description=read('README.md'),
     license="Apache License, Version 2.0",
     author="Gregor von Laszewski, cloudmesh.org",
     author_email="laszewski@gmail.com",

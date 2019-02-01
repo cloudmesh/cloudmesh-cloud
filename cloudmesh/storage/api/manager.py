3e07d397f1b561c7c710516b25fcfed6a6243b3e
Gregor von Laszewski <laszewski@gmail.com>
2019-02-01 15:31:58 -0500

    improve manager

--- cloudmesh/storage/api/manager.py
+++ cloudmesh/storage/api/manager.py
@@ -6,18 +6,29 @@ class Manager(object):
     def __init__(self):
         print("init {name}".format(name=self.__class__.__name__))
 
-    def list(self, parameter):
-        print("list", parameter)
-
-    def delete(self, filename):
-        print ("delete filename")
-
-    def get(self, service, filename):
-        print("get", service, filename)
+    def _provider(self, service):
+        provider = None
         if service == "gdrive":
             provider = cloudmesh.storage.provider.gdrive.Provider.Provider()
         elif service == "box":
             provider = cloudmesh.storage.provider.box.Provider.Provider()
+        return provider
 
+    def list(self, parameter):
+        print("list", parameter)
+        provider = self._provider(service)
+
+    def delete(self, filename):
+        print ("delete filename")
+        provider = self._provider(service)
+        provider.delete(filename)
+
+    def get(self, service, filename):
+        print("get", service, filename)
+        provider = self._provider(service)
         provider.get(filename)
 
+    def put(self, service, filename):
+        print("put", service, filename)
+        provider = self._provider(service)
+        provider.put(filename)

4998c7fc60f4550570d759a9c2aa990fbef16ba3
Gregor von Laszewski <laszewski@gmail.com>
2019-02-01 15:16:40 -0500

    add gdive and box provider templates

--- cloudmesh/storage/api/manager.py
+++ cloudmesh/storage/api/manager.py
@@ -1,4 +1,5 @@
 import cloudmesh.storage.provider.gdrive.Provider
+import cloudmesh.storage.provider.box.Provider
 
 class Manager(object):
 
@@ -15,6 +16,8 @@ class Manager(object):
         print("get", service, filename)
         if service == "gdrive":
             provider = cloudmesh.storage.provider.gdrive.Provider.Provider()
+        elif service == "box":
+            provider = cloudmesh.storage.provider.box.Provider.Provider()
 
         provider.get(filename)
 

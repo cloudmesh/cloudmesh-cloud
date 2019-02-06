ab8da4df0c8c1c8315662555f7e1bd3cf7a971be
Gregor von Laszewski <laszewski@gmail.com>
2019-02-06 03:29:06 -0500

    add a database Print decorator

--- cloudmesh/mongo/DataBaseDecorator.py
+++ cloudmesh/mongo/DataBaseDecorator.py
@@ -2,6 +2,7 @@ from cloudmesh.mongo.CmDatabase import CmDatabase
 from pprint import pprint
 from cloudmesh.management.configuration.name import Name
 from datetime import datetime
+from cloudmesh.common.util import banner
 
 class DatabaseUpdate:
     """
@@ -61,6 +62,39 @@ class DatabaseAdd:
             if result is not None:
                 result["created"] = result["updated"] = str(datetime.utcnow())
                 r = self.database.update(result, collection=self.collection, replace=self.replace)
+
+            return result
+        return wrapper
+
+class DatabasePrint:
+    """
+    Prints the dict of the result but does not add it to the DB
+
+    Example:
+
+        @DatabaseUpdate("test-collection")
+        def foo(x):
+            return {"test": "hello"}
+    """
+    def __init__(self, collection="cloudmesh", replace=False):
+        self.name = Name()
+
+    def __call__(self, f):
+        def wrapper(*args, **kwargs):
+            result = f(*args, **kwargs)
+            result["cmid"] = str(self.name)
+            result["cmcounter"] = str(self.name.counter)
+            result["created"] = result["updated"] = str(datetime.utcnow())
+
+
+            banner (result["cmid"], c=".")
+
+            if result is not None:
+                result["created"] = result["updated"] = str(datetime.utcnow())
+                pprint(result)
+            else:
+                print (None)
+            print (70 * ".")
 
             return result
         return wrapper

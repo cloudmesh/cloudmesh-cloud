c0cfeab35e713b10172cf3d9dabb1415c1af7b28
Gregor von Laszewski <laszewski@gmail.com>
2019-02-24 09:19:45 -0500

    improve key documentation

--- cloudmesh/key/command/key.py
+++ cloudmesh/key/command/key.py
@@ -17,71 +17,109 @@ class KeyCommand(PluginCommand):
 
            Usage:
              key  -h | --help
-             key list --cloud=CLOUD
-             key list --source=db [--format=FORMAT]
-             key list --source=yaml [--format=FORMAT]
+             key list --cloud=CLOUDS [--format=FORMAT]
              key list --source=ssh [--dir=DIR] [--format=FORMAT]
              key list --source=git [--format=FORMAT] [--username=USERNAME]
-             key list
+             key list [--format=FORMAT]
              key load [--format=FORMAT]
              key add [NAME] [--source=FILENAME]
              key add [NAME] [--git]
              key add [NAME] [--ssh]
-             key get NAME
+             key get NAME [--format=FORMAT]
              key default --select
-             key delete (NAME | --select | --all)
-             key delete NAME --cloud=CLOUD
-             key upload [NAME] [--cloud=CLOUD]
-             key upload [NAME] --active
-           Manages the keys
+             key delete (NAMES | --select | --all) [--dryrun]
+             key delete NAMES --cloud=CLOUDS [--dryrun]
+             key upload [NAMES] [--cloud=CLOUDS] [--dryrun]
+             key upload [NAMES] [VMS] [--dryrun]
+             key group upload [--group=GROUPNAMES] [--cloud=CLOUDS] [--dryrun]
+             key group add [--group=GROUPNAMES] [--cloud=CLOUDS] [--dryrun]
+             key group delete [--group=GROUPNAMES] [NAMES] [--dryrun]
+             key group list [--group=GROUPNAMES] [--format=FORMAT]
+
+
            Arguments:
-             CLOUD          The cloud
+             VMS            Para,eterized list of virtual machines
+             CLOUDS         The clouds
              NAME           The name of the key.
              SOURCE         db, ssh, all
              KEYNAME        The name of a key. For key upload it defaults to the default key name.
              FORMAT         The format of the output (table, json, yaml)
              FILENAME       The filename with full path in which the key
                             is located
-             NAME_ON_CLOUD  Typically the name of the keypair on the cloud.
+
            Options:
               --dir=DIR                     the directory with keys [default: ~/.ssh]
               --format=FORMAT               the format of the output [default: table]
               --source=SOURCE               the source for the keys [default: db]
               --username=USERNAME           the source for the keys [default: none]
               --name=KEYNAME                The name of a key
-              --all                         delete all keys
-              --force                       delete the key form the cloud
-              --name_on_cloud=NAME_ON_CLOUD Typically the name of the keypair on the cloud.
+
+
            Description:
-           key list --source=git  [--username=USERNAME]
-              lists all keys in git for the specified user. If the
-              name is not specified it is read from cloudmesh.yaml
-           key list --source=ssh  [--dir=DIR] [--format=FORMAT]
-              lists all keys in the directory. If the directory is not
-              specified the default will be ~/.ssh
-           key list --source=yaml  [--dir=DIR] [--format=FORMAT]
-              lists all keys in cloudmesh.yaml file in the specified directory.
-               dir is by default ~/.cloudmesh
-           key list [--format=FORMAT]
-               list the keys in the giiven format: json, yaml,
-               table. table is default
-           key list
-                Prints list of keys. NAME of the key can be specified
-           key add ssh
-               adds the default key with the name id_rsa.pub
-           key add NAME  --source=FILENAME
-               adds the key specifid by the filename to the key
-               database
-           key get NAME
-               Retrieves the key indicated by the NAME parameter from database
-               and prints its fingerprint.
-           key default --select
-                Select the default key interactively
-           key delete NAME
-                deletes a key. In yaml mode it can delete only key that
-                are not saved in the database
-           key rename NAME NEW
-                renames the key from NAME to NEW.
+
+               Please note that some values are read from the cloudmesh4.yaml
+               file. One such value is cloudmesh.profile.user
+
+               Manages public keys is an essential component of accessing
+               virtual machine sin the cloud. There are a number of sources
+               where you can find public keys. This includes teh ~/.ssh
+               directory and for example github. To list these keys the
+               following list functions are provided.
+
+                key list --source=git  [--username=USERNAME]
+                    lists all keys in git for the specified user. If the
+                    name is not specified it is read from cloudmesh4.yaml
+                key list --source=ssh  [--dir=DIR] [--format=FORMAT]
+                    lists all keys in the directory. If the directory is not
+                    specified the default will be ~/.ssh
+
+               The keys will be uploaded into cloudmesh with the add command
+               under the given name. If the name is not specified the name
+               cloudmesh.profile.user is assumed.
+
+                key add --ssh
+                    adds the default key in ~/.ssh/id_rsa.pub
+                key add NAME  --source=FILENAME
+                    adds the key specifid by the filename with the given name
+                key add NAME --git --username=username
+                    adds a named github key from a user with the given github
+                    username.
+
+                Once the keys are uploaded to github, they can be listed
+
+                key list [NAME] [--format=FORMAT]
+                    list the keys loaded to cloudmesh in the giiven format:
+                    json, yaml, table. table is default. The NAME cabn be
+                    specified and if ommitted the name cloudmesh.profile.user
+                    is assumed.
+
+                key get NAME
+                    Retrieves the key indicated by the NAME parameter from
+                    cloudmesh and prints its details.
+                key default --select
+                    Select the default key interactively
+                key delete NAMES
+                    deletes the keys. This may also have an impact on groups
+                key rename NAME NEW
+                    renames the key from NAME to NEW.
+
+               Group management of keys is an important concept in cloudmesh,
+               allowing multiple users to be added to virtual machines.
+               The keys must be uploaded to cloudmesh with a name so they can
+               be used in a group. The --dryrun option executes the command
+               without uploading the information to the clouds. If no groupname
+               is specified the groupname default is assumed. If no cloudnames
+               are specified, all active clouds are assumed. active clouds can be
+               set in the cloudmesh4.yaml file.
+
+                key group delete [GROUPNAMES] [NAMES] [--dryrun]
+                    deletes the named keys from the named groups.
+
+                key group list [GROUPNAMES] [--format=FORMAT]
+                    list the key names and details in the group.
+
+                key group upload [GROUPNAMES] [CLOUDS] [--dryrun]
+                    uploads the named groups to the specified clouds.
 
         """
 

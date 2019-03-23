2b84758c84de4eae7a9da34ac56b3754cc1a85a3
Gregor von Laszewski <laszewski@gmail.com>
2019-03-23 10:03:34 -0400

    replace decodestring with decodebytes

--- cloudmesh/management/configuration/SSHkey.py
+++ cloudmesh/management/configuration/SSHkey.py
@@ -165,7 +165,7 @@ class SSHkey(dict):
         try:
 
             keytype, key_string, comment = self._parse(keystring)
-            data = base64.decodestring(key_string)
+            data = base64.decodebytes(key_string)
             int_len = 4
             str_len = struct.unpack('>I', data[:int_len])[0]
             # this should return 7

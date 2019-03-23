d6aebdf7a1d6c24e117a843cba6fff9990dffe92
Gregor von Laszewski <laszewski@gmail.com>
2019-03-23 08:53:45 -0400

    add the storage abc class

---
+++ cloudmesh/abstractclass/StorageABC.py
@@ -0,0 +1,10 @@
+from abc import ABCMeta, abstractmethod
+
+
+# noinspection PyUnusedLocal
+class StorageABC(metaclass=ABCMeta):
+
+    def __init__(self, cloud, config):
+        pass
+
+# TODO: add the methods

9e17bb976b63efb1dd00a4d6b5afb8924c2df283
Gregor von Laszewski <laszewski@gmail.com>
2019-04-06 02:21:11 -0400

    Bump version: 4.0.19 → 4.0.20

--- docs-source/source/conf.py
+++ docs-source/source/conf.py
@@ -27,7 +27,7 @@ copyright = '2018, Cloudmesh Community'
 author = 'Cloudmesh Community'
 
 # The short X.Y version
-version = '4.0.19'
+version = '4.0.20'
 # The full version, including alpha/beta/rc tags
 release = 'alpha'
 

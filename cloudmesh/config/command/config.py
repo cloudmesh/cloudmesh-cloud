10ad91ec47fcb6438dfa1703f64d7a084ea8a1c9
Gregor von Laszewski <laszewski@gmail.com>
2019-04-06 23:47:34 -0400

    remove verbose print

--- cloudmesh/config/command/config.py
+++ cloudmesh/config/command/config.py
@@ -91,7 +91,7 @@ class ConfigCommand(PluginCommand):
                            path_expand("~/.cloudmesh/cloudmesh4.yaml")
         arguments.DESTINATION = arguments.SOURCE + ".enc"
 
-        # VERBOSE.print(arguments, verbose=9)  
+        # VERBOSE.print(arguments, verbose=9)
 
         e = EncryptFile(arguments.SOURCE, arguments.DESTINATION)
 

6c796db5d4c6853e6749831062395dcb2bbbaf8e
Gregor von Laszewski <laszewski@gmail.com>
2019-04-08 16:49:24 -0400

    print name of the parameter to VERBOSE

--- cloudmesh/DEBUG.py
+++ cloudmesh/DEBUG.py
@@ -1,9 +1,15 @@
 from cloudmesh.shell.variables import Variables
 from cloudmesh.common.util import banner
 from pprint import pformat
-
+from pprint import pprint
+import inspect
 
 def VERBOSE(msg, verbose=9, label=None, color="BLUE"):
+
+    if label is None:
+        label = inspect.stack()[1][4][0].strip().replace("VERBOSE(", "")
+        label = label.split(",")[0][:-1]
+
     _verbose = int(Variables()["verbose"] or 0)
     if _verbose >= verbose:
         banner(pformat(msg), label=label, color=color)

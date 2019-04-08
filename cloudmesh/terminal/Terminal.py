e04a96e3746b930f3c0eed3ba21c57a16d72c4c1
Gregor von Laszewski <laszewski@gmail.com>
2019-04-08 09:23:05 -0400

    make the devault verbose print 9

--- cloudmesh/terminal/Terminal.py
+++ cloudmesh/terminal/Terminal.py
@@ -6,7 +6,7 @@ from pprint import pformat
 class VERBOSE(object):
 
     @staticmethod
-    def print(msg, verbose=0, label=None, color="BLUE"):
+    def print(msg, verbose=9, label=None, color="BLUE"):
         _verbose = int(Variables()["verbose"] or 0)
         if _verbose >= verbose:
             banner(pformat(msg), label=label, color=color)

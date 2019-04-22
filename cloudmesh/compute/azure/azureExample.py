83ac99d6ebd007f095ee47e6c9d83b7e36943e63
Gregor von Laszewski <laszewski@gmail.com>
2019-04-22 19:34:44 -0400

    lets use the banner

--- cloudmesh/compute/azure/azureExample.py
+++ cloudmesh/compute/azure/azureExample.py
@@ -3,6 +3,7 @@ from libcloud.compute.providers import get_driver
 
 from libcloud.compute.drivers.azure_arm import AzureNodeDriver
 from libcloud.compute.base import NodeAuthSSHKey
+from cloudmesh.common.util import banner
 
 cls = get_driver(Provider.AZURE_ARM)
 driver = cls(tenant_id='...',
@@ -14,26 +15,26 @@ driver = cls(tenant_id='...',
 
 auth = NodeAuthSSHKey('ssh-rsa ...')
 
-print("%s%s%s" % ('*' * 30, 'locations/regions', '*' * 30))
+banner('locations/regions')
 locs = driver.list_locations()
 print(locs)
 
-print("%s%s%s" % ('*' * 30, 'resource groups', '*' * 30))
+banner('resource groups')
 resgroups = driver.ex_list_resource_groups()
 print(resgroups)
 
 # resource group need to be created via azure portal
 resgroup = 'cmtest'
 
-print("%s%s%s" % ('*' * 30, 'public IPs', '*' * 30))
+banner('public IPs')
 pubips = driver.ex_list_public_ips(resgroup)
 print(pubips)
 
-print("%s%s%s" % ('*' * 30, 'node sizes (list first 10)', '*' * 30))
+banner('node sizes (list first 10)')
 sizes = driver.list_sizes()
 print(sizes[:10])
 
-print("%s%s%s" % ('*' * 30, 'images', '*' * 30))
+banner('images')
 # images = driver.list_images()
 # print (images[:10])
 print("...too long to retrieve and load all images")
@@ -77,37 +78,37 @@ print(images)
 # similar to what need to be done in openstack
 # before the first use
 #
-print("%s%s%s" % ('*' * 30, 'networks', '*' * 30))
+banner('networks')
 networks = driver.ex_list_networks()
 print(networks)
 
-print("%s%s%s" % ('*' * 30, 'subnets', '*' * 30))
+banner('subnets')
 subnets = driver.ex_list_subnets(networks[0])
 print(subnets)
 
-print("%s%s%s" % ('*' * 30, 'nics', '*' * 30))
+banner('nics')
 nics = driver.ex_list_nics()
 print(nics)
 
-print("%s%s%s" % ('*' * 30, 'security groups', '*' * 30))
+banner('security groups')
 secgroups = driver.ex_list_network_security_groups(resgroup)
 print(secgroups)
 
-print("%s%s%s" % ('*' * 30, 'List public IPs', '*' * 30))
+banner('List public IPs')
 pubips = driver.ex_list_public_ips(resgroup)
 print(pubips)
 
-print("%s%s%s" % ('*' * 30, 'create a public IP', '*' * 30))
+banner('create a public IP')
 pubip = driver.ex_create_public_ip(name='pubip1',
                                    resource_group=resgroup
                                    )
 print(pubip)
 
-print("%s%s%s" % ('*' * 30, 'List public IPs', '*' * 30))
+banner('List public IPs')
 pubips = driver.ex_list_public_ips(resgroup)
 print(pubips)
 
-print("%s%s%s" % ('*' * 30, 'nics', '*' * 30))
+banner('nics')
 nic = driver.ex_create_network_interface(name='cminternal',
                                          subnet=subnets[0],
                                          resource_group=resgroup,
@@ -122,7 +123,7 @@ imageid = 'Canonical:UbuntuServer:18.04-LTS:18.04.201903200'
 size = [s for s in sizes if s.id == sizeid][0]
 image = driver.get_image(imageid)
 
-print("%s%s%s" % ('*' * 30, 'node creation', '*' * 30))
+banner('node creation')
 node = driver.create_node(name="fwpytest",
                           size=size,
                           image=image,
@@ -138,6 +139,6 @@ node = driver.create_node(name="fwpytest",
                           )
 print(node)
 
-print("%s%s%s" % ('*' * 30, 'nodes', '*' * 30))
+banner('nodes')
 nodes = driver.list_nodes()
 print(nodes)

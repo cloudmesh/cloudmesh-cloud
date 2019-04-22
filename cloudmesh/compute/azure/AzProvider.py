9fcc40a6e0cd45178cbdb47f2ed1b401571e0ea6
Gregor von Laszewski <laszewski@gmail.com>
2019-04-22 13:17:34 -0400

    remove resource group parameter

--- cloudmesh/compute/azure/AzProvider.py
+++ cloudmesh/compute/azure/AzProvider.py
@@ -41,7 +41,7 @@ class Provider(ComputeNodeABC):
         configuration = path_expand(configuration)
         conf = Config(name, configuration)["cloudmesh"]
 
-        super().__init__(name, configuration)
+        # super().__init__(name, configuration)
 
 
 

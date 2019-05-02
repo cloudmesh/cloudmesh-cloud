e48cd5115e4b89aad7525b70c0e73f7e542d0922
Gregor von Laszewski <laszewski@gmail.com>
2019-05-02 03:47:50 -0400

    add a static method that checks a file for \t which is useful to simplify debugging for config files

--- cloudmesh/management/configuration/config.py
+++ cloudmesh/management/configuration/config.py
@@ -56,6 +56,35 @@ class Config(object):
 
             copyfile(source.resolve(), self.config_path)
 
+    @staticmethod
+    def check_for_tabs(filename, verbose=True):
+        """identifies if the file contains tabs and returns True if it
+        does. It also prints the location of the lines and columns. If
+        verbose is set to False, the location is not printed.
+
+        :param verbose: if true prints issues
+        :param filename: the filename
+        :type filename: str
+        :rtype: True if there are tabs in the file
+        """
+        filename = path_expand(filename)
+        file_contains_tabs = False
+
+        with open(filename, 'r') as f:
+            lines = f.read().split("\n")
+
+        line_no = 1
+        for line in lines:
+            if "\t" in line:
+                file_contains_tabs = True
+                location = [
+                    i for i in range(len(line)) if line.startswith('\t', i)]
+                if verbose:
+                    Console.error(f"Tab found in line {line_no} "
+                                  f"and column(s) {location}")
+            line_no += 1
+        return file_contains_tabs
+
     def __init__(self, config_path='~/.cloudmesh/cloudmesh4.yaml',
                  encrypted=False):
         """

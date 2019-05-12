089a30072093a86e31dd1405e14aa66f4197327d
Gregor von Laszewski <laszewski@gmail.com>
2019-05-12 18:44:55 -0400

    remove batch examples

--- examples/batch/1_argsin_stdout_script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "hi $1"
-for i in {1..10}
-do
-	echo "Press [CTRL+C] to stop.."
-	sleep 1
-done
--- examples/vcluster/sample_scripts/inf_script_stdin_fileout.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for i in {1..10}
-do
-	echo "Press [CTRL+C] to stop.." >> ./outie.out
-	sleep 1
-done

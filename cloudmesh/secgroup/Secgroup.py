e34bdbeae9a87681c3870b24d7ef9dd472bf6b3c
Gregor von Laszewski <laszewski@gmail.com>
2019-07-12 17:19:42 -0400

    add pseudo code for the secgroup classes

--- cloudmesh/secgroup/Secgroup.py
+++ cloudmesh/secgroup/Secgroup.py
@@ -1,27 +1,99 @@
-from cloudmesh.mongo.DataBaseDecorator import DatabaseUpdate
-from cloudmesh.common.debug import VERBOSE
-from pprint import pprint
+from cloudmesh.common.parameter import Parameter
 
+#
+# use CmDatabase for interacting with the db, you will find also a simple find
+# function there.
+#
+
+class SecgroupRule(object):
+
+    def __init__(self):
+        pass
+
+    def add(self, rule, fromport, toport, protocol, cidr):
+        raise NotImplementedError
+
+    def delete(self, rule=None):
+        rules = Parameter.expand(rule)
+        # delete the rules
+        for rule in rules:
+            # delete the rule in the db
+            raise NotImplementedError
+
+        raise NotImplementedError
+
+    def list(self, rule=None):
+        found = []
+        if rule is None:
+            # find all rules in the db
+            found = []
+            raise NotImplementedError
+        else:
+            rules = Parameter.expand(rule)
+            # find only the rules specified in the db
+            find = []
+            raise NotImplementedError
+        found = self.update_dict_list(entries)
+        return found
+
+    def update_dict_list(self, entries, name):
+        for entry in entries:
+            entry['cm'] = {
+                "kind": "secgroup",
+                "name": entry['name'],
+                "cloud": "local",
+                "type": "rule"
+            }
+        return entries
 
 class Secgroup(object):
 
     def __init__(self):
         pass
 
-    @DatabaseUpdate()
-    def add (self, group, rule,  fromport, toport, protocol, cidr):
-        data = {
-            "cm": {
+    def add(self, group, rule):
+        """
+        adds a rule to a given group. If the group does not exist, it will be created.
+
+        :param group:
+        :param rule:
+        :return:
+        """
+        entry = None # find the entry in the db
+        found = self.update_dict_list(entry)
+        raise NotImplementedError
+
+    def delete(self, group=None):
+        """
+        deletes the groups
+        :param group:
+        :return:
+        """
+        groups = Parameter.expand(group)
+        for group in groups:
+            # delete the group in the db
+            raise NotImplementedError
+
+    def list(self, group=None):
+        found = []
+        if group is None:
+            # find all groups in the db
+            found = []
+            raise NotImplementedError
+        else:
+            groups = Parameter.expand(group)
+            # find only the grous specified in the db
+            find = []
+            raise NotImplementedError
+        found = self.update_dict_list(entries)
+        return found
+
+    def update_dict_list(self, entries, name):
+        for entry in entries:
+            entry['cm'] = {
                 "kind": "secgroup",
-                "cloud": "secgroup",
-                "name": f"{group}-{rule}",
-            },
-            "rule": rule,
-            "fromport": fromport,
-            "toport": toport,
-            "protocol": protocol,
-            "cdir": cidr
-        }
-        return data
-
-
+                "name": entry['name'],
+                "cloud": "local",
+                "type": "group"
+            }
+        return entries

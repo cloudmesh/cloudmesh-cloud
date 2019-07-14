2316f217450c33536d652fbee24fb2e35659d039
Saurabh Swaroop <saurabh.bmas@gmail.com>
2019-07-14 16:28:43 -0700

    Replaced yaml file configuration with Config()

--- cloudmesh/compute/awsboto/test_boto.py
+++ cloudmesh/compute/awsboto/test_boto.py
@@ -1,120 +1,103 @@
-####################################################################################
-#            A W S    E C 2  I N S T A N C E  A U T O M A T I O N                  #
-#                                                                                  #
-#     AUTHOR: SAURABH SWAROOP                                                      #
-#             INDIANA UNIVERSITY                                                   #
-####################################################################################                                   
+# python cloudmesh/compute/awsboto/test_boto.py
+
+
 import boto3
 import yaml
 import os
 import traceback
 import sys
+from pprint import pprint
 
+from cloudmesh.management.configuration.config import Config
 
-############### Loads Credentials Yaml file and returns all aws users
+config = Config()
+# pprint(config.data)
+credentials = config['cloudmesh.cloud.awsboto.credentials']
+pprint(credentials)
+new_yaml_data_dict = {
+    'User_5': {
+        'Access Key Id': 2,
+        'Secret Access Key Id': 'user2',
+        'region name': 'us-west',
+    }
 
-def yaml_loader(filepath):
-	with open(filepath,"r") as file_descriptor:
-		data = yaml.safe_load(file_descriptor)
-	return data
+}
+# update_credential(new_yaml_data_dict,filepath)
+'''
+credentials:
+        region: 'us-west-2'
+        EC2_SECURITY_GROUP: 'group1'
+        EC2_ACCESS_ID: TBD
+        EC2_SECRET_KEY: TBD
+        EC2_PRIVATE_KEY_FILE_PATH: '~/.cloudmesh/aws_cert.pem'
+        EC2_PRIVATE_KEY_FILE_NAME: 'aws_cert'
+'''
+try:
 
-############## Adds new user to Credentials Yaml file
+    session = boto3.Session(
+        aws_access_key_id=credentials['EC2_ACCESS_ID'],
+        aws_secret_access_key=credentials['EC2_SECRET_KEY'],
+        region_name=credentials['region'])
+except:
+    print("Access Key is not correct...Please try again..")
+    sys.exit()
+if session:
+# ec2_instance, ec2_ids = list_ec2_instance_id(session)
+print(ec2_ids)
 
-def update_credential(new_data,filepath):
+sys.exit()
 
-
-	with open(filepath,'r') as yamlfile:
-		credentials = yaml.safe_load(yamlfile)
-		credentials.update(new_data)
-	if credentials:
-		with open(filepath,'w') as updatedfile:
-			yaml.safe_dump(credentials,updatedfile,default_flow_style = False)
 
 # List all EC2 instance ID's in a particular region for a user
 
 def list_ec2_instance_id(session):
-	ec2_instance = session.resource('ec2')
-	instance_ids = []
-	for each_instance in ec2_instance.instances.all():
-		instance_ids.append(each_instance.id)
-	return ec2_instance,instance_ids
+    ec2_instance = session.resource('ec2')
+    instance_ids = []
+    for each_instance in ec2_instance.instances.all():
+        instance_ids.append(each_instance.id)
+    return ec2_instance, instance_ids
 
-########## Returns status of an EC2 instance
+
+# Returns status of an EC2 instance
 
 def get_ec2_instance_status(ec2_instance):
-	state = []
-	for each in ec2_instance.instances.all():
-		print(each)
-		state.append(each.id.state)
-	return state
+    state = []
+    for each in ec2_instance.instances.all():
+        print(each)
+        state.append(each.id.state)
+    return state
 
-	
-	return instance.state
-
-########## Starts an EC2 instance
-
-def start_ec2_instance(session,instance_id):
-
-	ec2 = session.client('ec2')
-	print(instance_id)
-	try:
-		ec2.start_instances(InstanceIds=[instance_id])
-		print("Instance starting..")
-		ec2.wait_until_running()
-		print("Intance started")
-	except Exception as ex:
-		traceback.print_exc()
-		
-########## Stops an EC2 instance       
-
-def stop_ec2_instance(session,instance_id):
-
-	ec2 = session.client('ec2')
-	
-	try:
-		ec2.stop_instances(InstanceIds=[instance_id])
-		print("Instance stopping..")
-		
-	except:
-		print("EC2 Instance stopping failed...Try again..")
-        
+    return instance.state
 
 
-if __name__ == "__main__":
+# Starts an EC2 instance
 
-	filepath = "credentials.yaml"
-	data = yaml_loader(filepath)
-	
-	for key, value in data.items() :
-		if value is not None:
-			print(value['Access Key Id'])
-			print(value['Secret Access Key Id'])
-
-	new_yaml_data_dict = {
-		'User_5':{
-			'Access Key Id': 2, 
-            'Secret Access Key Id': 'user2', 
-            'region name': 'us-west', 
-		}
-
-	}
-	# update_credential(new_yaml_data_dict,filepath)
+def start_ec2_instance(session, instance_id):
+    ec2 = session.client('ec2')
+    print(instance_id)
+    try:
+        ec2.start_instances(InstanceIds=[instance_id])
+        print("Instance starting..")
+        ec2.wait_until_running()
+        print("Intance started")
+    except Exception as ex:
+        traceback.print_exc()
 
 
-	try:
-		
-		session = boto3.Session(aws_access_key_id=data['User_1']['Access Key Id'],
-                                         aws_secret_access_key=data['User_1']['Secret Access Key Id'],region_name = data['User_1']['region name'])
-	except:
-		print("Access Key is not correct...Please try again..")
-		sys.exit()
-	if session:
-		ec2_instance,ec2_ids = list_ec2_instance_id(session)
-	print(ec2_ids)
+# Stops an EC2 instance
 
-	instance_state = get_ec2_instance_status(ec2_instance)
-	print("currently instance state is {instance_state")
+def stop_ec2_instance(session, instance_id):
+    ec2 = session.client('ec2')
 
-	#start_ec2_instance(session,ec2_ids[0])
-	#stop_ec2_instance(session,ec2_ids[0])
+    try:
+        ec2.stop_instances(InstanceIds=[instance_id])
+        print("Instance stopping..")
 
+    except:
+        print("EC2 Instance stopping failed...Try again..")
+
+    instance_state = get_ec2_instance_status(ec2_instance)
+    print("currently instance state is {instance_state")
+
+# start_ec2_instance(session,ec2_ids[0])
+# stop_ec2_instance(session,ec2_ids[0])

9cadbecd58407ab0f60907ef7854aaf96fe44cba
Vafa Andalibi <vafa.andalibi@gmail.com>
2019-07-15 15:58:06 -0400

    modularization of aws register yaml

--- cloudmesh/register/AWSRegister.py
+++ cloudmesh/register/AWSRegister.py
@@ -3,12 +3,128 @@ import random
 import getpass
 from time import sleep
 from cloudmesh.common.console import Console
-import selenium as sel
+from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+from sys import platform
+import os
+import subprocess
+import yaml
+from pathlib import Path, PurePath, PurePosixPath, PureWindowsPath
+import pandas
 
 class AWSRegister(object):
 
-    def __init__(self, driver):
-        self.driver = driver
+    def __init__(self):
+        pass
+
+    def register(self):
+        if platform == "linux" or platform == "linux2":
+            # check if chrome installed
+
+            chrome_ver1 = subprocess.getoutput('google-chrome-stable --version')
+            chrome_ver2 = subprocess.getoutput('google-chrome --version')
+            if 'not found' in chrome_ver1.lower() or 'not found' in chrome_ver2.lower():
+                Console.error("google chrome is not installed")
+                return
+            # self.driver = webdriver.Chrome()
+            try:
+                self.driver = webdriver.Chrome()
+                # register = AWSRegister(self.driver)
+            except WebDriverException:
+                Console.error("Chrome geckodriver not installed. Follow these steps for installation: \n"
+                              "1) Download the driver from the following link: \n\t "
+                              "https://sites.google.com/a/chromium.org/chromedriver/downloads \n"
+                              "2) Copy the `chromedriver` to '/usr/bin' \n"
+                              "3) Set the permission using:\n\t"
+                              "'sudo chmod +x /usr/bin/chromedriver'")
+                return
+            self.create_user()
+            credentials_csv_path = Path.home().joinpath('Downloads').joinpath('credentials.csv').resolve()
+            cloudmesh_folder = Path.home().joinpath('.cloudmesh').resolve()
+            os.rename(credentials_csv_path, cloudmesh_folder.joinpath('credentials.csv').resolve())
+            Console.info("credentials.csv moved to ~/.cloudmesh folder")
+
+            with open("{cm}/cloudmesh4.yaml".format(cm=cloudmesh_folder)) as f:
+                cloudmesh_conf = yaml.load(f, Loader=yaml.FullLoader)
+
+            creds = pandas.read_csv("{cm}/credentials.csv".format(cm=cloudmesh_folder))
+
+            cloudmesh_conf['cloudmesh']['cloud']['aws']['credentials']['EC2_ACCESS_ID'] = \
+                creds['Access key ID'][0]
+            cloudmesh_conf['cloudmesh']['cloud']['aws']['credentials']['EC2_SECRET_KEY'] = \
+                creds['Secret access key'][0]
+
+            with open("{cm}/cloudmesh4.yaml".format(cm=cloudmesh_folder), "w") as f:
+                yaml.dump(cloudmesh_conf, f)
+
+            Console.info("AWS 'Access Key ID' and 'Secret Access Key' in the cloudmesh4.yaml updated")
+
+        elif platform == "darwin":
+            chrome = Path("/Applications/Google Chrome.app/Contents/MacOS/Google Chrome")
+            if not chrome.is_file():
+                Console.error("Google chrome is not installed")
+                return
+            try:
+                self.driver = webdriver.Chrome()
+            except WebDriverException:
+                Console.error("Chrome geckodriver not installed. Follow these steps for installation: \n"
+                              "1) Download the driver from the following link: \n\t "
+                              "https://sites.google.com/a/chromium.org/chromedriver/downloads \n"
+                              "2) Copy the `chromedriver` to '/usr/local/bin' \n"
+                              "3) Set the permission using:\n\t"
+                              "'chmod +x /usr/local/bin/chromedriver'")
+                return
+            self.create_user()
+            credentials_csv_path = Path.home().joinpath('Downloads').joinpath('credentials.csv').resolve()
+            cloudmesh_folder = Path.home().joinpath('.cloudmesh').resolve()
+            os.rename(credentials_csv_path, cloudmesh_folder.joinpath('credentials.csv').resolve())
+            Console.info("credentials.csv moved to ~/.cloudmesh folder")
+
+            with open("{cm}/cloudmesh4.yaml".format(cm=cloudmesh_folder)) as f:
+                cloudmesh_conf = yaml.load(f, Loader=yaml.FullLoader)
+
+            creds = pandas.read_csv("{cm}/credentials.csv".format(cm=cloudmesh_folder))
+
+            cloudmesh_conf['cloudmesh']['cloud']['aws']['credentials']['EC2_ACCESS_ID'] = creds['Access key ID'][0]
+            cloudmesh_conf['cloudmesh']['cloud']['aws']['credentials']['EC2_SECRET_KEY'] = creds['Secret access key'][0]
+
+            with open("{cm}/cloudmesh4.yaml".format(cm=cloudmesh_folder), "w") as f:
+                yaml.dump(cloudmesh_conf, f)
+
+            Console.info("AWS 'Access Key ID' and 'Secret Access Key' in the cloudmesh4.yaml updated")
+
+
+        elif platform == "win32":
+            chrome = Path("C:\Program Files (x86)\Google\Chrome\Application\Chrome.exe")
+            if not chrome.is_file():
+                Console.error("Google chrome is not installed")
+                return
+            try:
+                self.driver = webdriver.Chrome()
+            except WebDriverException:
+                Console.error("Chrome geckodriver not installed. Follow these steps for installation: \n"
+                              "1) Download the driver from the following link: \n\t "
+                              "https://sites.google.com/a/chromium.org/chromedriver/downloads \n"
+                              "2) Copy the `chromedriver` to path, for instance you can add it to the followtin path: \n\t %USERPROFILE%\AppData\Local\Microsoft\WindowsApps")
+                return
+            self.create_user()
+            credentials_csv_path = Path.home().joinpath('Downloads').joinpath('credentials.csv').resolve()
+            cloudmesh_folder = Path.home().joinpath('.cloudmesh').resolve()
+            os.rename(credentials_csv_path, cloudmesh_folder.joinpath('credentials.csv').resolve())
+            Console.info("credentials.csv moved to ~/.cloudmesh folder")
+
+            with open("{cm}/cloudmesh4.yaml".format(cm=cloudmesh_folder)) as f:
+                cloudmesh_conf = yaml.load(f, Loader=yaml.FullLoader)
+
+            creds = pandas.read_csv("{cm}/credentials.csv".format(cm=cloudmesh_folder))
+
+            cloudmesh_conf['cloudmesh']['cloud']['aws']['credentials']['EC2_ACCESS_ID'] = creds['Access key ID'][0]
+            cloudmesh_conf['cloudmesh']['cloud']['aws']['credentials']['EC2_SECRET_KEY'] = creds['Secret access key'][0]
+
+            with open("{cm}/cloudmesh4.yaml".format(cm=cloudmesh_folder), "w") as f:
+                yaml.dump(cloudmesh_conf, f)
+
+            Console.info("AWS 'Access Key ID' and 'Secret Access Key' in the cloudmesh4.yaml updated")
 
     def slow_typer(self,element, text):
         for character in text:
@@ -24,7 +140,7 @@ class AWSRegister(object):
                     "Captcha encountered. Please enter the captcha and press Submit then press Enter to continue")
 
 
-    def create_user(self, driver):
+    def create_user(self):
         email = input("Enter your email: ")
         passw = getpass.getpass("Enter your password: ")
         self.driver.get("https://console.aws.amazon.com/iam/home#/users")

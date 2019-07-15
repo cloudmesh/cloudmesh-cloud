ef3e0467663dd35ad7e8feeca9058335f7d806e5
Gregor von Laszewski <laszewski@gmail.com>
2019-07-15 16:29:09 -0400

    remove identical script for win32 and linux and osx for set_auth in mongo

--- cloudmesh/mongo/MongoDBController.py
+++ cloudmesh/mongo/MongoDBController.py
@@ -300,17 +300,16 @@ class MongoDBController(object):
         add admin account into the MongoDB admin database
         """
 
-        if platform.lower() == 'win32':
-            script = """
-            mongo --eval "db.getSiblingDB('admin').createUser({{user:'{MONGO_USERNAME}',pwd:'{MONGO_PASSWORD}',roles:[{{role:'root',db:'admin'}}]}})"
-            """.format(**self.data)
-            print(script)
-        else:
-            script = """mongo --eval 'db.getSiblingDB("admin").createUser({{user:"{MONGO_USERNAME}",pwd:"{MONGO_PASSWORD}",roles:[{{role:"root",db:"admin"}}]}})'""".format(
-                **self.data)
+        script = """mongo --eval 'db.getSiblingDB("admin").createUser({{user:"{MONGO_USERNAME}",pwd:"{MONGO_PASSWORD}",roles:[{{role:"root",db:"admin"}}]}})'""".format(**self.data)
 
         result = Script.run(script)
-        print(result)
+        if "Successfully added user" in result:
+            Console.ok("Administrative user created.")
+        else:
+            Console.error("Problem creating the administrative user. Check "
+                          "the yaml file and make sure the password and "
+                          "username are not TBD.")
+        # print(result)
 
     def dump(self, filename):
         """

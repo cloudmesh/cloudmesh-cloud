65e8468632ef684dfafefd65ac5fef4da7c5a334
Gregor von Laszewski <laszewski@gmail.com>
2019-07-15 11:14:36 -0400

    remove openstack libcloud provider, add vm boot ability, improve the name test

--- tests/test_name.py
+++ tests/test_name.py
@@ -7,42 +7,80 @@ from pprint import pprint
 from cloudmesh.common.util import HEADING
 from cloudmesh.management.configuration.name import Name
 import pytest
-
-
+import os
+from cloudmesh.common.util import path_expand
 # nosetest -v --nopature
 # nosetests  tests/test_name.py
+
+path = path_expand("~/.cloudmesh/name.yaml")
+data = {
+    'counter': 1,
+    'path': path,
+    'kind': "vm",
+    'schema': "{experiment}-{group}-{user}-{kind}-{counter}",
+    'experiment': 'exp',
+    'group': 'group',
+    'user': 'user'
+}
+
+try:
+    os.remove(path)
+except:
+    pass
+
+n = None
 
 @pytest.mark.incremental
 class TestName:
 
-    def setup(self):
-        pass
+    def test_define(self):
+        n=Name()
+        assert dict(data) == n.dict()
 
-    def test_Name(self):
-        HEADING()
+    def test_define_new(self):
+        os.remove(path)
 
-        n = Name(experiment="exp",
-                 group="grp",
+        n = Name(schema="{user}-{kind}-{counter}",
+                 counter="3",
                  user="gregor",
-                 kind="vm",
-                 counter=1)
+                 kind="vm")
+        data = n.dict()
+        pprint(data)
+        assert data == dict({'counter': 3,
+                             'kind': 'vm',
+                             'path': '/Users/grey/.cloudmesh/name.yaml',
+                             'schema': '{user}-{kind}-{counter}',
+                             'user': 'gregor'})
 
+
+    def test_name_reset(self):
+        n = Name()
         n.reset()
         assert n.counter == 1
 
+    def test_name_print_str(self):
+        n = Name()
         print(n)
-        assert str(n) == "exp-grp-gregor-vm-1"
+        assert str(n) == "gregor-vm-1"
 
+    def test_name_dict(self):
+        n = Name()
         pprint(n.dict())
+        data = n.dict()
+        assert data == dict({'counter': 1,
+                             'kind': 'vm',
+                             'path': '/Users/grey/.cloudmesh/name.yaml',
+                             'schema': '{user}-{kind}-{counter}',
+                             'user': 'gregor'})
 
-        print(n.get("container"))
-        print(n)
-        assert str(n) == "exp-grp-gregor-container-1"
-
+    def test_name_incr(self):
+        n=Name()
         n.incr()
         print(n)
-        assert str(n) == "exp-grp-gregor-container-2"
+        assert str(n) == "gregor-vm-2"
 
+    def test_name_counter(self):
+        n = Name()
         print(n.counter)
         assert n.counter == 2
 

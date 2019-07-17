52c47ea6b39ee691845853bc026bd0954b30f6dc
Gregor von Laszewski <laszewski@gmail.com>
2019-07-17 10:58:19 -0400

    move the secgroup local api test to 1_basic
    Signed-off-by: Gregor von Laszewski <laszewski@gmail.com>

---
+++ tests/cloud/test_sec_command.py
@@ -0,0 +1,136 @@
+###############################################################
+# pytest -v --capture=no tests/1_basic/test_sec_command.py
+# pytest -v  tests/1_basic/test_sec_command.py
+###############################################################
+#
+# The following commands are tested on the local database
+#
+# cms sec clear
+# cms sec load
+# cms rule add
+# cms sec rule delete deleteme
+# cms sec group add deleteme empty empty
+# cms sec group delete deleteme
+# cms sec rule list
+# cms sec list
+# cms sec group list
+#
+###############################################################
+
+
+import pytest
+from cloudmesh.common.StopWatch import StopWatch
+from cloudmesh.common.util import HEADING
+from cloudmesh.common3.Shell import Shell
+from cloudmesh.secgroup.Secgroup import Secgroup
+from cloudmesh.secgroup.Secgroup import SecgroupExamples
+from cloudmesh.secgroup.Secgroup import SecgroupRule
+
+rules = SecgroupRule()
+groups = Secgroup()
+
+
+def run(label, command):
+    result = Shell.run_timed(label, command, service="local")
+    print(result)
+    return result
+
+
+@pytest.mark.incremental
+class TestSecCLI:
+
+    def test_clear(self):
+        HEADING(color="HEADER")
+
+        run("clear", "cms sec clear")
+
+        r = rules.list()
+        g = groups.list()
+
+        assert len(r) == 0
+        assert len(g) == 0
+
+    def test_load(self):
+        run("load", "cms sec load")
+
+        r = rules.list()
+        g = groups.list()
+
+        assert len(r) > 0
+        assert len(g) > 0
+
+        examples = SecgroupExamples()
+        assert len(g) == len(examples.secgroups)
+        assert len(r) == len(examples.secrules)
+
+    def test_rule_add(self):
+        HEADING()
+
+        result = run("rule add",
+                     f"cms sec rule add deleteme FROMPORT TOPORT PROTOCOL CIDR")
+        entry = rules.list(name="deleteme")
+
+        assert len(entry) > 0
+        assert entry[0]["name"] == "deleteme"
+
+    def test_rule_delete(self):
+        HEADING()
+
+        result = run("rule delete", f"cms sec rule delete deleteme")
+        entry = rules.list(name="deleteme")
+
+        assert len(entry) == 0
+
+    def test_group_add(self):
+        HEADING()
+
+        result = run("group add", f"cms sec group add deleteme empty empty")
+        entry = groups.list(name="deleteme")
+
+        assert len(entry) > 0
+        assert entry[0]["name"] == "deleteme"
+
+    def test_group_delete(self):
+        HEADING()
+
+        result = run("group delete", f"cms sec group delete deleteme")
+        entry = groups.list(name="deleteme")
+
+        assert len(entry) == 0
+
+    def test_rule_list(self):
+        HEADING()
+
+        result = run("rule list", "cms sec rule list")
+        r = rules.list()
+        g = groups.list()
+
+        for entry in r:
+            name = entry['name']
+            assert name in result
+        for entry in g:
+            name = entry['name']
+            assert name in result
+
+    def test_list(self):
+        HEADING()
+
+        result = run("list", "cms sec list")
+        g = groups.list()
+
+        for entry in g:
+            name = entry['name']
+            assert name in result
+
+    def test_group_list(self):
+        HEADING()
+
+        result = run("list", "cms sec group list")
+        g = groups.list()
+
+        for entry in g:
+            name = entry['name']
+            assert name in result
+
+    def test_benchmark(self):
+        StopWatch.benchmark()

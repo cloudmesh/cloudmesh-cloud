634c533d2d93337bfaed690ecbfe05eaae8766fa
Gregor von Laszewski <laszewski@gmail.com>
2019-07-19 01:11:14 -0400

    template on how to get all tables in a dict of tables

--- cloudmesh/compute/aws/gregor_flavor_aws.py
+++ cloudmesh/compute/aws/gregor_flavor_aws.py
@@ -18,11 +18,19 @@ def read():
     return content
 
 
+
 input = read()
+input = input.replace("<th", "<td")
+
+
 soup = BeautifulSoup(input, features="lxml")
 
-REMOVE_ATTRIBUTES = ['style', 'height', 'width', 'border', 'cellpadding',
-'cellspacing']
+REMOVE_ATTRIBUTES = ['style',
+                     'height',
+                     'width',
+                     'border',
+                     'cellpadding',
+                     'cellspacing']
 for attribute in REMOVE_ATTRIBUTES:
     for tag in soup.find_all(attrs={attribute: True}):
         del tag[attribute]
@@ -35,12 +43,28 @@ for tag in TAGS:
 
 tables = soup.findAll("table")
 
+all = {}
 for table in tables:
      if table.findParent("table") is None:
          print()
-         banner("Table")
          content = str(table)
          #pretty = BeautifulSoup(content).prettify()
          #print(pretty)
-         print (content)
+         # print (content)
+
+         output = []
+         table_rows = table.find_all('tr')
+         for tr in table_rows:
+             td = tr.find_all('td')
+             row = [i.text.strip() for i in td]
+             output.append(row)
+         kind = output[1][0].split(".",1)[0]
+         all[kind] = output
+
+for kind in all:
+    banner(f"Table {kind}")
+    pprint (all[kind])
+
+pprint(all)
+
 

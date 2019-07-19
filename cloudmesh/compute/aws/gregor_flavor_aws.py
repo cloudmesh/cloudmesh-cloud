23011b06a22914e2f2ac83502a8536927167283b
Gregor von Laszewski <laszewski@gmail.com>
2019-07-19 05:31:41 -0400

    convert to class

--- cloudmesh/compute/aws/gregor_flavor_aws.py
+++ cloudmesh/compute/aws/gregor_flavor_aws.py
@@ -7,65 +7,73 @@ from textwrap import dedent
 from bs4 import BeautifulSoup
 from cloudmesh.common.util import banner
 
-def fetch():
-    url='https://aws.amazon.com/ec2/instance-types/'
-    r = requests.get(url)
-    print (r)
-    writefile("gregor_flavor_aws.html", r.content.decode("utf-8"))
+class AwsImages(object):
 
-def read():
-    content = readfile("gregor_flavor_aws.html")
-    return content
+    def __init__(self):
+        self.html = self.fetch()
+        self.data = self.extract_tables(self.html)
+
+    def fetch(self):
+        url='https://aws.amazon.com/ec2/instance-types/'
+        r = requests.get(url)
+        return r.content.decode("utf-8")
+
+    def extract_tables(self, html):
+        html = html.replace("<th", "<td")
+        html = html.replace("vCPU*", "vCPU")
+
+        soup = BeautifulSoup(html, features="lxml")
+
+        REMOVE_ATTRIBUTES = ['style',
+                             'height',
+                             'width',
+                             'border',
+                             'cellpadding',
+                             'cellspacing']
+        for attribute in REMOVE_ATTRIBUTES:
+            for tag in soup.find_all(attrs={attribute: True}):
+                del tag[attribute]
+
+        TAGS = ['b', 'i', 'u']
+        for tag in TAGS:
+            for match in soup.findAll(tag):
+                match.replaceWithChildren()
 
 
+        tables = soup.findAll("table")
 
-input = read()
-input = input.replace("<th", "<td")
+        all = {}
+        for table in tables:
+             if table.findParent("table") is None:
+                 print()
+                 content = str(table)
+                 #pretty = BeautifulSoup(content).prettify()
+                 #print(pretty)
+                 # print (content)
 
+                 output = []
+                 table_rows = table.find_all('tr')
+                 for tr in table_rows:
+                     td = tr.find_all('td')
+                     row = [i.text.strip() for i in td]
+                     output.append(row)
+                 kind = output[1][0].split(".",1)[0]
+                 all[kind] = output
 
-soup = BeautifulSoup(input, features="lxml")
+        return all
 
-REMOVE_ATTRIBUTES = ['style',
-                     'height',
-                     'width',
-                     'border',
-                     'cellpadding',
-                     'cellspacing']
-for attribute in REMOVE_ATTRIBUTES:
-    for tag in soup.find_all(attrs={attribute: True}):
-        del tag[attribute]
+    def print(self):
+        for kind in self.data:
+            banner(f"Table {kind}")
+            pprint (self.data[kind])
 
-TAGS = ['b', 'i', 'u']
-for tag in TAGS:
-    for match in soup.findAll(tag):
-        match.replaceWithChildren()
+    def pprint(self):
+        banner('pprint')
+        pprint(all)
 
-
-tables = soup.findAll("table")
-
-all = {}
-for table in tables:
-     if table.findParent("table") is None:
-         print()
-         content = str(table)
-         #pretty = BeautifulSoup(content).prettify()
-         #print(pretty)
-         # print (content)
-
-         output = []
-         table_rows = table.find_all('tr')
-         for tr in table_rows:
-             td = tr.find_all('td')
-             row = [i.text.strip() for i in td]
-             output.append(row)
-         kind = output[1][0].split(".",1)[0]
-         all[kind] = output
-
-for kind in all:
-    banner(f"Table {kind}")
-    pprint (all[kind])
-
-banner('pprint')
-pprint(all)
+if __name__ == "__main__":
+     images = AwsImages()
+     images.print()
+     images.pprint()
 
 

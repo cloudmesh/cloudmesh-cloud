cf4bccbe1aceac9196b9bbe77c45e0b0f893e9d3
Gregor von Laszewski <laszewski@gmail.com>
2019-07-19 22:37:48 -0400

    clenup documentation

--- cloudmesh/common3/DictList.py
+++ cloudmesh/common3/DictList.py
@@ -1,8 +1,45 @@
 from pprint import pprint
 
 class DictList(dict):
+    """
+    A class to convert lists of dicts to dicts.
+
+    Example:
+
+        data = [
+        {"name": "vm1", "status": "on"},
+        {"name": "vm2", "status": "on"},
+        {"name": "vm3", "status": "on"},
+        {"name": "vm4", "status": "on"},
+        ]
+
+        d = DictList(data)
+        pprint (d)
+
+        # {'vm1': {'name': 'vm1', 'status': 'on', 'x': 0},
+        #  'vm2': {'name': 'vm2', 'status': 'on', 'x': 1},
+        #  'vm3': {'name': 'vm3', 'status': 'on', 'x': 2},
+        #  'vm4': {'name': 'vm4', 'status': 'on', 'x': 3}}
+
+        print (d['vm1'])
+        # {'name': 'vm1', 'status': 'on', 'x': 0}
+        #
+        print (d.list())
+        # [{'name': 'vm1', 'status': 'on', 'x': 0},
+        #  {'name': 'vm2', 'status': 'on', 'x': 1},
+        #  {'name': 'vm3', 'status': 'on', 'x': 2},
+        #  {'name': 'vm4', 'status': 'on', 'x': 3}]
+
+
+    """
 
     def __init__(self, entries=None, key='name', position='x'):
+        """
+
+        :param entries: a list of dicte
+        :param key: a key that is used as name within the dict
+        :param position: the name of a key that stores the order
+        """
 
         if type (entries) == list:
             counter = 0

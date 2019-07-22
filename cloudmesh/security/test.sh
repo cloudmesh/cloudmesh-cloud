0147d91b9389bd6abc08982dafa9cce2fd753b9d
Gregor von Laszewski <laszewski@gmail.com>
2019-07-22 12:36:17 -0400

    code cleanup: cloudmesh.security

--- cloudmesh/security/test.sh
+++ cloudmesh/security/test.sh
@@ -3,22 +3,24 @@
 CERT=key
 CERT=id_rsa
 
-#openssl genrsa -out $CERT.pem 1024
+# openssl genrsa -out $CERT.pem 1024
+
 ls $CERT* pub*
 openssl rsa -in $CERT.pem -text -noout
 ls $CERT* pub*
+
 # Save public key in pub.pem file:
 
 openssl rsa -in $CERT.pem -pubout -out pub.pem
 ls $CERT* pub*
 openssl rsa -in pub.pem -pubin -text -noout
 
-#3) Encrypt some data:
+# Encrypt some data:
 
 echo test test test > file.txt
 openssl rsautl -encrypt -inkey pub.pem -pubin -in file.txt -out file.bin
 ls $CERT* pub*
 
-#4) Decrypt encrypted data:
+# Decrypt encrypted data:
 
 openssl rsautl -decrypt -inkey key.pem -in file.bin

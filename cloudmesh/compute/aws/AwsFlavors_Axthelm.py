8d4448dc60348f89847f159c44f848c010b4ae4f
Alex Axthelm <alex.axthelm@semanticbits.com>
2019-08-01 17:05:31 -0400

    Add json parsing

--- cloudmesh/compute/aws/AwsFlavors_Axthelm.py
+++ cloudmesh/compute/aws/AwsFlavors_Axthelm.py
@@ -1,42 +1,58 @@
-import urllib.request
+from cloudmesh.compute.generic.Provider import flavor
+from cloudmesh.management.configuration.config import Config
 import contextlib
-import re
+import urllib.request
+import json
 
-def gather_aws_ec2_instance_types(
-        instance_type_url="https://aws.amazon.com/ec2/instance-types/"
-):
+class AWSflavor(flavor):
 
-    with contextlib.closing(urllib.request.urlopen(instance_type_url)) as x:
-        raw_html = x.read().decode("utf8")
-    # remove formatting html
-    raw_html = re.sub(pattern = "<b>|</b>|<br />", repl = "", string = raw_html)
+    def update(self, dict = {}):
+        # Note that dict is overwritten, and will be ignored
+        offer_file = self.fetch_offer_file()
+        dict = self.parse_offer_file(offer_file)
+        self.__dict__ = dict
 
-    # search for tables
-    tables = re.findall(pattern = "<table.*?</table>", string = raw_html, flags=re.DOTALL)
+    @staticmethod
+    def fetch_json_file(url):
+        with urllib.request.urlopen(url) as req:
+            data = json.loads(req.read().decode())
+            return data
 
-    output = []
-    for t in tables:
-        # check that headers exists
-        model_header = (re.search("Model", t) is not None)
-        vcpu_header = (re.search("vCPU", t) is not None)
-        mem_header = (re.search("Mem(ory)* \([GT]iB\)", t) is not None)
-        if (model_header and vcpu_header and mem_header):
-            rows = re.findall(pattern = "<tr>.*?</tr>", string = t, flags=re.DOTALL)
-            for idx_r, row in enumerate(rows):
-                cols = re.findall(pattern = "<td.*?>(.*?)</td>", string = row, flags=re.DOTALL)
-                if idx_r == 0 :
-                    headers = cols
-                else:
-                    output.append(dict(zip(headers, cols)))
-    return output
+    def fetch_offer_file(
+            self, 
+            url = None,
+            region = "us-east-1"
+    ):
+        if url is None:
+            offer_index_url = "https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/index.json"
+            offer_index = self.fetch_json_file(offer_index_url)
+            offer_file_api_url = "https://pricing.us-east-1.amazonaws.com"
+            # offer_file_path = offer_index['offers']['AmazonEC2']['currentVersionUrl']
+            region_file_path = offer_index['offers']['AmazonEC2']['currentRegionIndexUrl']
+            regions_url = offer_file_api_url + region_file_path
+            regions_file = self.fetch_json_file(regions_url)
+            offer_file_path = regions_file["regions"][region]["currentVersionUrl"]
+            url = offer_file_api_url + offer_file_path
+        offer_file = self.fetch_json_file(url)
+        return offer_file
 
-def gather_aws_ec2_prices(
-        region="reg-us-east",
-        pricing_url = "https://aws.amazon.com/ec2/spot/pricing/"
-):
-
-    with contextlib.closing(urllib.request.urlopen(pricing_url)) as x:
-        raw_html = x.read().decode("utf8")
-    # remove formatting html
-    raw_html = re.sub(pattern = "<b>|</b>", repl = "", string = raw_html)
-
+    def parse_offer_file(self, offer_file):
+        publication_date = offer_file['publicationDate']
+        flavor_info = {}
+        for sku in list(offer_file['terms']['OnDemand'].keys()):
+            for offer_term in list(offer_file['terms']['OnDemand'][sku].keys()):
+                for rate_code in list(offer_file['terms']['OnDemand'][sku][offer_term]['priceDimensions'].keys()):
+                    flavor = {
+                        'vcpu': offer_file['products'][sku]['attributes'].get('vcpu'),
+                        'memory': offer_file['products'][sku]['attributes'].get('memory'),
+                        'storage': offer_file['products'][sku]['attributes'].get('storage'),
+                        'clock_speed': offer_file['products'][sku]['attributes'].get('clockSpeed'),
+                        'instance_type': offer_file['products'][sku]['attributes'].get('InstanceType'),
+                        'price':float(offer_file["terms"]["OnDemand"][sku][offer_term]['priceDimensions'][rate_code]['pricePerUnit']['USD']),
+                        'additional_info': [
+                            offer_file['products'][sku],
+                            offer_file["terms"]["OnDemand"][sku][offer_term]
+                        ]
+                    }
+                    flavor_info[rate_code] = flavor
+        return flavor_info

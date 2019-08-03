044c90739e8f9d2a98d3734bfb0e6dd278968ec6
Gregor von Laszewski <laszewski@gmail.com>
2019-08-03 16:03:33 -0400

    add simple time wrapper

--- cloudmesh/common3/DateTime.py
+++ cloudmesh/common3/DateTime.py
@@ -1,32 +1,63 @@
 import datetime as TIME
-import humanize
+from datetime import timezone
+import humanize as HUMANIZE
 from dateutil import parser
+import time
 
 class DateTime(object):
 
     timezone = TIME.timezone
 
+
     @staticmethod
     def now():
         return TIME.datetime.now()
 
-    def natural(self, time):
+    @staticmethod
+    def natural(time):
         return parser.parse(time)
 
-    def datetime(self, time):
+    @staticmethod
+    def datetime(time):
         if type (time) == TIME:
             return time
         else:
-            return humanize(time)
+            return DateTime.humanize(time)
         pass
 
-    def humanize(self, time):
-        pass
+    @staticmethod
+    def humanize(time):
+        return HUMANIZE.naturaltime(time)
 
-    def string(self, time):
+    @staticmethod
+    def string(time):
         if type(time) == str:
             d = time
         else:
             d = TIME.datetime.date(time)
         d = DateTime.humanize(time)
         return d
+
+    @staticmethod
+    def delta(n):
+        return TIME.timedelta(seconds=n)
+
+    @staticmethod
+    def local(time):
+        return time
+
+    @staticmethod
+    def utc(time):
+        return time.replace(tzinfo=timezone.utc).astimezone(tz=None)
+
+
+if __name__ == "__main__":
+
+    start = DateTime.now()
+    stop = DateTime.now() + DateTime.delta(1)
+
+    print ("START", start)
+    print ("STOP", stop)
+    print("HUMANIZE STOP", DateTime.humanize(stop - start))
+    print ("LOCAL", DateTime.local(start))
+    print("UTC", DateTime.utc(start))

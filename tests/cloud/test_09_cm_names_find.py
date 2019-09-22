eadcf43bd968c1dd4a4bb8c41a7771458e8e38c6
Gregor von Laszewski <laszewski@gmail.com>
2019-09-22 04:58:06 -0400

    remove frame debug message

--- tests/cloud/test_09_cm_names_find.py
+++ tests/cloud/test_09_cm_names_find.py
@@ -25,14 +25,18 @@ class Test_cm_find:
 
     def test_cm_find_collection(self):
         HEADING()
+        Benchmark.Start()
         entries = cm.find(collection=f"{cloud}-vm")
+        Benchmark.Stop()
         print(entries)
         assert len(entries) > 0
 
     def test_cm_find_loop(self):
         HEADING()
+        Benchmark.Start()
         for kind in ['vm', "image", "flavor"]:
             entries = cm.find(cloud=f"{cloud}", kind=kind)
+        Benchmark.Stop()
         # pprint(entries)
         assert len(entries) > 0
 
@@ -45,17 +49,24 @@ class Test_cm_find:
 
     def test_cm_image_name_cloud(self):
         HEADING()
+        Benchmark.Start()
         names = cm.names(cloud=cloud, kind="image")
+        Benchmark.Stop()
         # pprint(names)
         assert len(names) > 0
 
     def test_cm_image_name_collection(self):
         HEADING()
+        Benchmark.Start()
         names = cm.names(collection=f"{cloud}-image")
+        Benchmark.Stop()
+
         # pprint(names)
 
     def test_names_regexp(self):
         HEADING()
+        Benchmark.Start()
+
         names = cm.names(collection=f"{cloud}-image", regex="^CC-")
         print(cloud, names)
         for entry in names:
@@ -67,10 +78,15 @@ class Test_cm_find:
         for entry in names:
             print(entry)
             assert "Ubuntu" in entry
+        Benchmark.Stop()
+
 
     def test_cm_find_vms(self):
         HEADING()
+        Benchmark.Start()
         entries = cm.find(cloud=f"{cloud},azure", kind="vm")
+        Benchmark.Stop()
+
         pprint(entries)
 
     def test_cm_find_ubuntu_in_images(self):
@@ -79,7 +95,10 @@ class Test_cm_find:
         query = {"name": {'$regex': ".*Ubuntu.*"}}
         print(query)
 
+        Benchmark.Start()
         entries = cm.find(collection=f"{cloud}-image", query=query)
+        Benchmark.Stop()
+
         print(entries)
         assert len(entries) > 0
 
@@ -91,35 +110,50 @@ class Test_cm_find:
         HEADING()
         print()
 
+        Benchmark.Start()
         entries = cm.find(collection=f"{cloud}-vm",
                           attributes=['cm.cloud', 'cm.name'])
+        Benchmark.Stop()
+
         print(entries)
         assert len(entries) > 0
 
     def test_cm_find_cloud_name_attributes(self):
         HEADING()
         print()
-
+        Benchmark.Start()
         entries = cm.find(collection=f"{cloud}-vm",
                           attributes=['cm.cloud', 'cm.name'])
+        Benchmark.Stop()
+
         print(entries)
         assert len(entries) > 0
 
     def test_cm_find_vm_collections(self):
         HEADING()
+        Benchmark.Start()
         collections = cm.collections()
+        Benchmark.Stop()
+
         print(collections)
         assert len(collections) > 0
 
+    def test_cm_find_vm_collections_vm(self):
+        HEADING()
+        Benchmark.Start()
         collections = cm.collections(regex=".*-vm")
+        Benchmark.Stop()
         print(collections)
         assert len(collections) > 0
 
     def test_cm_find_vm_collection_form_parameter(self):
         HEADING()
+        Benchmark.Start()
         collections = cm.collections(
             name="a-vm,b-vm",
             regex=".*-vm")
+        Benchmark.Stop()
+
         print(collections)
         assert len(collections) == 2
 

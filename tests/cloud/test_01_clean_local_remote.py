548a46406571b4049357a86c91869b12afefa17c
Gregor von Laszewski <laszewski@gmail.com>
2019-10-03 10:51:11 -0400

    disable the removal of all vms in the benchmark

--- tests/cloud/test_01_clean_local_remote.py
+++ tests/cloud/test_01_clean_local_remote.py
@@ -77,22 +77,22 @@ class Test_Clean_Local_Remote:
             print(e)
             assert False
 
-    def test_terminate_all_instances(self):
-        HEADING()
-        try:
-            instances = provider.list()
-            Benchmark.Start()
-            for instance in instances:
-                if cloud == 'aws':
-                    instance_name = instance['name']
-                elif cloud == 'chameleon':
-                    instance_name = instance['name']
-                r = provider.destroy(instance_name)
-                print(r)
-            Benchmark.Stop()
-        except Exception as e:
-            print(e)
-            assert False
+    # def test_terminate_all_instances(self):
+    #     HEADING()
+    #     try:
+    #         instances = provider.list()
+    #         Benchmark.Start()
+    #         for instance in instances:
+    #             if cloud == 'aws':
+    #                 instance_name = instance['name']
+    #             elif cloud == 'chameleon':
+    #                 instance_name = instance['name']
+    #             r = provider.destroy(instance_name)
+    #             print(r)
+    #         Benchmark.Stop()
+    #     except Exception as e:
+    #         print(e)
+    #         assert False
 
     def test_benchmark(self):
         Benchmark.print(sysinfo=False, csv=False, tag=cloud)

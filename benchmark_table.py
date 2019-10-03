8cb31adbdbe88ab21de0eef2aafcd7f63427a4ea
Gregor von Laszewski <laszewski@gmail.com>
2019-10-03 15:43:49 -0400

    add a simple benchmark printer

---
+++ benchmark_table.py
@@ -0,0 +1,27 @@
+from cloudmesh.common.util import readfile
+from cloudmesh.common.Shell import Shell
+from pprint import pprint
+from tabulate import tabulate
+import sys
+
+filename = sys.argv[1]
+
+lines = readfile(filename).splitlines()
+#print (lines)
+
+result = Shell.find_lines_with(lines, "#csv")
+
+table = []
+for line in result:
+    values = line.split(",")
+    table.append([values[1],
+                  values[2],
+                  values[6],
+                  values[7],
+                  values[8]
+                  ]
+                 )
+
+pprint(table)
+
+print(tabulate(table, tablefmt="grid"))

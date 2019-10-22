dd82bb2ab453fb5cddf82268e2af184e73692e64
Gregor von Laszewski <laszewski@gmail.com>
2019-10-22 08:40:10 -0400

    start of the docker mongo class

--- cloudmesh/mongo/MongoDocker.py
+++ cloudmesh/mongo/MongoDocker.py
@@ -1,11 +1,15 @@
 from cloudmesh.configuration.Config import Config
 from cloudmesh.common.util import path_expand
+from cloudmesh.management.script import Script, SystemPath
+from cloudmesh.management.script import find_process
+
 
 class MongoDocker(object):
 
     def __init__(self, configuration="~/.cloudmesh/cloudmesh.yaml"):
         path = path_expand(configuration)
         self.config = Config(config_path=path)
+        self.data = self.config["cloudmesh.data.mongo"]
 
     def start(self):
         """
@@ -26,6 +30,19 @@ class MongoDocker(object):
         Creates the MongoDB Container
         :return:
         """
+        username = self.data["MONGO_USERNAME"]
+        password = self.data["MONGO_PASSWORD"]
+        port = self.data["MONGO_PORT"]
+        host = self.data["MONGO_HOST"]
+        script = \
+            f"docker run -d -p {host}:{port}:{port}" \
+            f" --name cloudmesh-mongo" \
+            f" -e MONGO_INITDB_ROOT_USERNAME={username}" \
+            f" -e MONGO_INITDB_ROOT_PASSWORD={password}" \
+            f" mongo"
+
+        installer = Script.run(script)
+        print(installer)
         raise NotImplementedError
 
     def create_admin(self):

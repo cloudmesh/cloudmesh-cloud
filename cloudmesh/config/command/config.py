198c6dcbb1faaddf2031c7bd1113799c3e731656
ElectricErudite <ElectricErudite@protonmail.com>
2019-11-27 23:08:36 -0500

    Delete enc and dec code handlede by Config class

--- cloudmesh/config/command/config.py
+++ cloudmesh/config/command/config.py
@@ -37,8 +37,8 @@ class ConfigCommand(PluginCommand):
              config cat [less]
              config check
              config secinit
-             config encrypt [SOURCE] 
-             config decrypt [SOURCE]
+             config encrypt 
+             config decrypt 
              config edit [ATTRIBUTE]
              config set ATTRIBUTE=VALUE
              config get ATTRIBUTE [--output=OUTPUT]
@@ -232,181 +232,12 @@ class ConfigCommand(PluginCommand):
             Config.check()
 
         elif arguments.encrypt:
-            """ 
-            Encrypts the keys listed within Config.secrets()
-
-            Assumptions:
-              1. ```cms init``` or ```cms config secinit``` has been executed
-              2. that the secidr is ~/.cloudmesh/security and exists [secinit]
-              3. Private key has same base name as public key
-              4. Public key ends with .pub, .pem, or any .[3 char combo]
-              5. Private key is in PEM format
-              6. The cloudmesh config version has not changed since encrypt
-                 This means data must re-encrypt upon every config upgrade
-            """
-
-            # Helper variables
             config = Config()
-            d = config.dict() # dictionary of config 
-            ch = CmsHasher() # Will hash the paths to produce file name
-            kh = KeyHandler() # Loads the public or private key bytes
-            ce = CmsEncryptor() # Assymmetric and Symmetric encryptor
-
-            #Create tmp file in case reversion is needed
-            named_temp = tempfile.NamedTemporaryFile(delete=True)
-            revertfd = open(named_temp.name, 'w') # open file for reading and writing
-            yaml.dump(config.data, revertfd) # dump file in yaml format
-            revertfd.close() # close the data fd used to backup reversion file 
-
-            # Secinit variables: location where keys are stored
-            cmssec_path = path_expand(config.get_value('cloudmesh.security.secpath'))
-            gcm_path = f"{cmssec_path}/gcm"
-
-            # Get the public key
-            kp = config.get_value('cloudmesh.security.publickey')
-            print(f"pub:{kp}")
-            pub = kh.load_key(kp, "PUB", "SSH", False)
-
-            # Get the regular expressions from config file
-            try:
-                secexps = config.get_value('cloudmesh.security.secrets')
-                flat_conf = flatten(config.data, sep='.')
-                keys = flat_conf.keys()
-                for e in secexps: # for each expression in section
-                    r = re.compile(e)
-                    paths = list( filter( r.match, keys ) )
-                    Console.ok( f"Expression:{e}")
-                    for path in paths: # for each path that reaches the key
-                        # Hash the path to create a base filename
-                        # MD5 is acceptable since security does not rely on hiding path
-                        h = ch.hash_data(path, "MD5", "b64", True)
-                        fp = f"{gcm_path}/{h}" #path to filename for key and nonce
-                        # Check if the attribute has already been encrypted
-                        if os.path.exists(f"{fp}.key"):
-                            Console.ok( f"\tAlready encrypted: {path}")
-                        else:
-                            Console.ok( f"\tencrypting: {path}")
-                            ## Additional Authenticated Data: the cloudmesh version
-                            # number is used to future-proof for version attacks 
-                            aad = config.get_value('cloudmesh.version')
-
-                            # Get plaintext data from config
-                            pt = config.get_value(path)
-                            b_pt = pt.encode()
-
-                            # Encrypt the cloudmesh.yaml attribute value
-                            k, n, ct = ce.encrypt_aesgcm(data =b_pt, aad = aad.encode())
-
-                            ## Write ciphertext contents
-                            ct = int.from_bytes(ct, "big")
-                            config.set(path, f"{ct}")
-
-                            # Encrypt symmetric key with users public key
-                            k_ct = ce.encrypt_rsa(pub = pub, pt = k)
-                            ## Write key to file
-                            k_ct = b64encode(k_ct).decode()
-                            fk = f"{fp}.key" # use hashed filename with indicator
-                            writefd(filename = fk , content = k_ct)
-
-                            # Encrypt nonce with users private key
-                            n_ct = ce.encrypt_rsa(pub = pub, pt = n)
-                            ## Write nonce to file
-                            n_ct = b64encode(n_ct).decode()
-                            fn = f"{fp}.nonce"
-                            writefd(filename = fn, content = n_ct)
-            except Exception as e:
-                Console.error("reverting cloudmesh.yaml")
-                copy2(src = named_temp.name, dst = config.config_path)
-                named_temp.close() #close (and delete) the reversion file
-                raise e
-
-            named_temp.close() #close (and delete) the reversion file
-            Console.ok("Success")
+            config.encrypt()
 
         elif arguments.decrypt:
-            """
-            Decrypts all secrets within the config file
-
-            Assumptions: please reference assumptions within encryption section above
-
-            Note: could be migrated to Config() directly
-
-            """
-            # Helper Classes 
             config = Config()
-            ch = CmsHasher() # Will hash the paths to produce file name
-            kh = KeyHandler() # Loads the public or private key bytes
-            ce = CmsEncryptor() # Assymmetric and Symmetric encryptor
-
-            #Create tmp file in case reversion is needed
-            named_temp = tempfile.NamedTemporaryFile(delete=True)
-            revertfd = open(named_temp.name, 'w') # open file for reading and writing
-            yaml.dump(config.data, revertfd) # dump file in yaml format
-            revertfd.close() # close the data fd used to backup reversion file 
-
-            # Secinit variables: location where keys are stored
-            cmssec_path = path_expand(config.get_value('cloudmesh.security.secpath'))
-            gcm_path = f"{cmssec_path}/gcm"
-
-            # Load the private key
-            kp = config.get_value('cloudmesh.security.privatekey')
-            prv = kh.load_key(kp, "PRIV", "PEM", True)
-
-            try:
-                # Get the regular expressions from config file
-                secexps = config.get_value('cloudmesh.security.secrets')
-                flat_conf = flatten(config.data, sep='.')
-                keys = flat_conf.keys()
-                for e in secexps: # for each expression in section
-                    r = re.compile(e)
-                    paths = list( filter( r.match, keys ) )
-                    Console.ok( f"Expression:{e}")
-                    for path in paths: # for each path that reaches the key
-                        # hash the path to find the file name
-                        # MD5 is acceptable, attacker gains nothing by knowing path
-                        h = ch.hash_data(path, "MD5", "b64", True)
-                        fp = f"{gcm_path}/{h}"
-                        if not os.path.exists(f"{fp}.key"):
-                            Console.ok( f"\tAlready plaintext: {path}" )
-                        else:
-                            Console.ok( f"\tDecrypting: {path}")
-                            # Decrypt symmetric key, using private key
-                            k_ct = readfile(f"{fp}.key")
-                            b_k_ct = b64decode(k_ct)
-                            b_k = ce.decrypt_rsa(priv = prv, ct = b_k_ct)
-
-                            # Decrypt nonce, using private key
-                            n_ct = readfile(f"{fp}.nonce")
-                            b_n_ct = b64decode(n_ct)
-                            b_n = ce.decrypt_rsa(priv = prv, ct = b_n_ct)
-
-                            # Version number was used as aad
-                            aad = config.get_value('cloudmesh.version')
-                            b_aad = aad.encode()
-
-                            # Read ciphertext from config
-                            ct = int(config.get_value(path))
-                            b_ct = ct.to_bytes((ct.bit_length() + 7) // 8, 'big')
-
-                            # Decrypt the attribute value ciphertext
-                            pt=ce.decrypt_aesgcm(key=b_k, nonce=b_n, aad=b_aad, ct=b_ct)
-                            pt = pt.decode()
-
-                            # Set the attribute with the plaintext value
-                            config.set(path, pt)
-
-                            #Remove old ciphers
-                            os.remove(f"{fp}.key")
-                            os.remove(f"{fp}.nonce")
-            except Exception as e:
-                Console.error("reverting cloudmesh.yaml")
-                copy2(src = named_temp.name, dst = config.config_path)
-                named_temp.close() #close (and delete) the reversion file
-                raise e
-
-            named_temp.close() #close (and delete) the reversion file
-
-            Console.ok("Success")
+            config.decrypt()
 
         elif arguments.ssh and arguments.verify:
 

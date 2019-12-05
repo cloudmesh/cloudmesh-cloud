5f72f5ffeb7b0d022a5a201bfd8dc7b78c249613
Gregor von Laszewski <laszewski@gmail.com>
2019-12-05 08:45:30 -0500

    add specialized status test for oracle

--- tests/cloud/test_08_vm_provider.py
+++ tests/cloud/test_08_vm_provider.py
@@ -63,8 +63,12 @@ class Test_provider_vm:
         name = str(Name())
         status = provider.status(name=name)[0]
         print(f'status: {str(status)}')
-        assert status["cm.status"] in ['ACTIVE', 'BOOTING', 'TERMINATED',
-                                       'STOPPED']
+        if cloud == 'oracle':
+            assert status["cm.status"] in ['STARTING', 'RUNNING', 'STOPPING',
+                                           'STOPPED']
+        else:
+            assert status["cm.status"] in ['ACTIVE', 'BOOTING', 'TERMINATED',
+                                           'STOPPED']
 
     def test_provider_vmprovider_vm_list(self):
         # list should be after create() since it would return empty and

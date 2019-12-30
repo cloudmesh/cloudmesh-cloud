0e66ea6c48adc0f34ace47aad3393f871fbc759c
Gregor von Laszewski <laszewski@gmail.com>
2019-12-30 11:41:42 -0500

    change common3 to common

--- cloudmesh/mongo/MongoDBController-new.py
+++ cloudmesh/mongo/MongoDBController-new.py
@@ -16,11 +16,11 @@ from cloudmesh.common.Shell import Shell, Brew
 from cloudmesh.common.console import Console
 from cloudmesh.common.dotdict import dotdict
 from cloudmesh.common.util import path_expand
-from cloudmesh.common3.Shell import Shell
+from cloudmesh.common.Shell import Shell
 from cloudmesh.configuration.Config import Config
 from cloudmesh.management.script import Script, SystemPath
 from cloudmesh.management.script import find_process
-from cloudmesh.common3.Shell import Shell as Shell3
+from cloudmesh.common.Shell import Shell
 from cloudmesh.common.debug import VERBOSE
 
 # from cloudmesh.mongo.MongoDBController import MongoDBController
@@ -575,7 +575,7 @@ class MongoDBController(object):
                 **self.data)
             # print(script)
             try:
-                # result = Shell3.run2(script)
+                # result = Shell.run2(script)
                 p = subprocess.Popen(script, shell=True, stdout=subprocess.PIPE,
                                      stderr=STDOUT)
                 result = p.stdout.read().decode('utf-8')

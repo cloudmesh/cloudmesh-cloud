d741cf9d4003254bdba567323fcc1f83eed65abb
Gregor von Laszewski <laszewski@gmail.com>
2020-03-04 16:46:23 -0500

    Bump version: 4.3.0 → 4.3.1

--- setup.py
+++ setup.py
@@ -105,7 +105,7 @@ setup(
     description=DESCRIPTION,
     long_description=long_description,
     long_description_content_type="text/markdown",
-    version="4.3.0",
+    version="4.3.1",
     license="Apache 2.0",
     url=URL,
     packages=find_packages(exclude=("tests",

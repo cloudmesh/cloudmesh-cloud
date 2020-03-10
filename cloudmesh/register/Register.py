4d5f7c1c4b608970fe86d4e6870904b751aa3ab1
Gregor von Laszewski <laszewski@gmail.com>
2020-03-10 06:14:35 -0400

    add volume register

--- cloudmesh/register/Register.py
+++ cloudmesh/register/Register.py
@@ -1,27 +1,96 @@
+from cloudmesh.common.console import Console
+
 class Register(object):
 
     def __init__(self):
         print("init {name}".format(name=self.__class__.__name__))
 
-    def aws(self, filename):
-        #
-        # do import
-        # have a uniform entry sample in each provider
-        # verify against entry
-        print("register", filename)
-        raise NotImplementedError
+    @staticmethod
+    def get_provider(service, kind):
+        """
+        Method to import the provider based on the service and kind.
+        :param service: Type of the service e.g. compute or storage etc.
+        :param kind: Name of the cloud e.g. google, azure, aws etc.
+        :return: Provider class
+        """
+        try:
+            if service == 'compute' or service == 'cloud':
 
-    def azure(self, filename):
-        print("register", filename)
+                if kind == 'openstack':
+                    from cloudmesh.openstack.compute.Provider import Provider
+                elif kind == 'azure':
+                    from cloudmesh.azure.compute.Provider import Provider
+                elif kind == 'aws':
+                    from cloudmesh.aws.compute.Provider import Provider
+                elif kind == 'oracle':
+                    from cloudmesh.oracle.compute.Provider import Provider
+                elif kind == 'google':
+                    from cloudmesh.google.compute.Provider import Provider
+                else:
+                    Console.error(
+                        f"No suitable provider found for {kind} and {service}")
+                    return None
 
-        raise NotImplementedError
+            elif service == 'storage':
 
-    def google(self, filename):
-        print("register", filename)
+                if kind == 'openstack':
+                    from cloudmesh.openstack.storage.Provider import Provider
+                elif kind == 'azure':
+                    from cloudmesh.storage.azure.Provider import Provider
+                elif kind == 'aws':
+                    from cloudmesh.storage.aws.Provider import Provider
+                elif kind == 'oracle':
+                    from cloudmesh.oracle.compute.Provider import Provider
+                elif kind == 'google':
+                    from cloudmesh.google.storage.Provider import Provider
+                else:
+                    Console.error(
+                        f"No suitable provider found for {kind} and {service}")
+                    return None
 
-        raise NotImplementedError
+            elif service == 'volume':
 
-    def chameleon(self, filename):
-        print("register", filename)
+                from cloudmesh.volume.Volume import Provider
+                p = Provider ()
+                return p.get(kind)
 
-        raise NotImplementedError
+            else:
+                Console.error(f"Invalid {service} provided.")
+                return None
+        except:
+            Console.error(f"Registration failed {service} and {kind}")
+            return None
+
+        p = Provider
+
+        return p
+
+    @staticmethod
+    def get_sample(provider, kind, service, name, attributes):
+
+        # Default replacements.
+        replacements = {'name': name,
+                        'service': service,
+                        "kind": kind}
+
+        # Add the attributes to the dict.
+        for attribute in attributes:
+            key, value = attribute.split("=")
+            replacements[key] = value
+
+        try:
+            # Extract the sample from Provider.
+            sample = provider.sample
+        except:
+            Console.error(f"Can not find the sample in the Provider")
+            return ""
+
+        try:
+            # Format the sample by replacing the attributes.
+            sample = sample.format(**replacements)
+
+        except KeyError as e:
+            Console.error(f"Value for {e} is not specified")
+            sample = None
+
+        return sample
--- cloudmesh/register/command/register.py
+++ cloudmesh/register/command/register.py
@@ -8,6 +8,7 @@ from cloudmesh.common.util import path_expand
 from cloudmesh.register.Entry import Entry
 from cloudmesh.shell.command import PluginCommand
 from cloudmesh.shell.command import command, map_parameters
+from cloudmesh.register.Register import Register
 
 class RegisterCommand(PluginCommand):
 
@@ -18,8 +19,13 @@ class RegisterCommand(PluginCommand):
         ::
 
             Usage:
-                register --cloud=CLOUD --service=SERVICE [--name=NAME] [--filename=FILENAME] [--keep] [ATTRIBUTES...] [--dryrun]
-                register CLOUD SERVICE [--name=NAME] [--filename=FILENAME] [--keep] [ATTRIBUTES...] [--dryrun]
+                register --cloud=CLOUD
+                         [--service=SERVICE]
+                         [--name=NAME]
+                         [--filename=FILENAME]
+                         [--keep]
+                         [ATTRIBUTES...]
+                         [--dryrun]
 
                 This command adds the registration information in the cloudmesh
                 yaml file. The permissions of the FILENAME will also be changed.
@@ -27,29 +33,29 @@ class RegisterCommand(PluginCommand):
                 be deleted after integration.
 
             Arguments:
-                CLOUD       cloud provider e.g. aws, google, openstack, oracle etc.
-                SERVICE     service type e.g. storage, compute etc.
                 FILENAME    a filename in which the cloud credentials are stored
                 ATTRIBUTES  Attribute list to replace if json file is not provided.
                             Note: Attributes will override the values from file
                             if both are used.
 
             Options:
-                --keep              keeps the file with the filename.
-                --dryrun            option to just display the formatted sample without
-                                    updating the cloudmesh.yaml file.
-                --filename=FILENAME json filename containing the details to be replaced
-                --cloud=CLOUD       cloud provider e.g. aws, google, openstack, oracle etc.
-                --service=SERVICE   service type e.g. storage, compute etc.
-                --name=NAME         name for the new registration
+                --keep               keeps the file with the filename.
+                --dryrun             option to just display the formatted sample without
+                                     updating the cloudmesh.yaml file.
+                --filename=FILENAME  json filename containing the details to be replaced
+                --cloud=CLOUD        cloud provider e.g. aws, google, openstack, oracle etc.
+                --service=SERVICE    service type e.g. storage, compute etc.
+                --name=NAME          name for the new registration
 
             Examples:
 
-                cms register google compute --name=west_region filename=~/.cloudmesh/google_west.json project_id=west1 client_email=example@gmail.com
+                cms register google compute --name=west_region \
+                    filename=~/.cloudmesh/google_west.json project_id=west1 \
+                    client_email=example@gmail.com
 
-                 In the last example the values for filename, project_id, and
-                 client_email will be changed to respective values from google
-                 compute sample.
+                  In the last example the values for filename, project_id, and
+                  client_email will be changed to respective values from google
+                  compute sample.
 
         """
 
@@ -63,7 +69,7 @@ class RegisterCommand(PluginCommand):
 
         kind = arguments.cloud
         service = arguments.service
-        entry_name = kind
+        entry_name = arguments.name or arguments.cloud
 
         """
         TODO: Check if this is required.
@@ -84,18 +90,7 @@ class RegisterCommand(PluginCommand):
             return ""
         """
 
-
-        # Extract arguments.
-        if kind is None:
-            kind = arguments.CLOUD
-
-        if service is None:
-            service = arguments.SERVICE
-
-        if arguments.name is not None:
-            entry_name = arguments.name
-
-        provider = self.get_provider(service, kind)
+        provider = Register.get_provider(service, kind)
 
         if provider is None:
             return
@@ -118,7 +113,10 @@ class RegisterCommand(PluginCommand):
             # Use Arguments.
             attributes = arguments.ATTRIBUTES
 
-        sample = self.prepare_sample(provider, kind, service, entry_name,
+        sample = Register.get_sample(provider,
+                                     kind,
+                                     service,
+                                     entry_name,
                                      attributes)
 
         if arguments.dryrun:
@@ -131,76 +129,8 @@ class RegisterCommand(PluginCommand):
                       path="~/.cloudmesh/cloudmesh.yaml")
 
         Console.ok(
-            f"Registered {service} service for {kind} provider with name {entry_name}.")
+            f"Registered {service} service for {kind}"
+            f" provider with name {entry_name}.")
         return ""
 
-    def get_provider(self, service, kind):
-        """
-        Method to import the provider based on the service and kind.
-        :param service: Type of the service e.g. compute or storage etc.
-        :param kind: Name of the cloud e.g. google, azure, aws etc.
-        :return: Provider class
-        """
-        if service == 'compute' or service == 'cloud':
-            if kind == 'openstack':
-                from cloudmesh.openstack.compute.Provider import Provider
-            elif kind == 'azure':
-                banner("Azure")
-                from cloudmesh.azure.compute.Provider import Provider
-            elif kind == 'aws':
-                from cloudmesh.aws.compute.Provider import Provider
-            elif kind == 'oracle':
-                from cloudmesh.oracle.compute.Provider import Provider
-            elif kind == 'google':
-                from cloudmesh.google.compute.Provider import Provider
-            else:
-                Console.error(
-                    f"No suitable provider found for {kind} and {service}")
-                return None
-        elif service == 'storage':
-            if kind == 'openstack':
-                from cloudmesh.openstack.storage.Provider import Provider
-            elif kind == 'azure':
-                from cloudmesh.storage.azure.Provider import Provider
-            elif kind == 'aws':
-                from cloudmesh.storage.aws.Provider import Provider
-            elif kind == 'oracle':
-                from cloudmesh.oracle.compute.Provider import Provider
-            elif kind == 'google':
-                from cloudmesh.google.storage.Provider import Provider
-            else:
-                Console.error(
-                    f"No suitable provider found for {kind} and {service}")
-                return None
-        else:
-            Console.error(f"Invalid {service} provided.")
-            return None
 
-        p = Provider
-
-        return p
-
-    def prepare_sample(self, provider, kind, service, name, attributes):
-
-        # Default replacements.
-        replacements = {'name': name,
-                        'service': service,
-                        "kind": kind}
-
-        # Add the attributes to the dict.
-        for attribute in attributes:
-            key, value = attribute.split("=")
-            replacements[key] = value
-
-        # Extract the sample from Provider.
-        sample = provider.sample
-
-        try:
-            # Format the sample by replacing the attributes.
-            sample = sample.format(**replacements)
-
-        except KeyError as e:
-            Console.error(f"Value for {e} is not specified")
-            sample = None
-
-        return sample

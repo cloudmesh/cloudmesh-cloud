e550037c434a07e877c016b36fd413c67d6cbcd6
Gregor von Laszewski <laszewski@gmail.com>
2020-03-31 16:14:30 -0400

    moved mongo, check, management, admin, source, start, stop to cloudmesh-admin

--- cloudmesh/mongo/test.sh
+++
@@ -1,8 +0,0 @@
-rm -rf ~/.cloudmesh/mongodb
-mkdir ~/.cloudmesh/mongodb
-mongod --port 27017 --dbpath ~/.cloudmesh/mongodb &
-sleep 1
-mongo admin --port 27017 --eval 'db.createUser(  {user: "admin", pwd: "changeme", roles: [ { role: "userAdminAnyDatabase", db: "admin" }, "readWriteAnyDatabase" ]  }); db.adminCommand( { shutdown: 1 } );'
-mongod --auth --port 27017 --dbpath ~/.cloudmesh/mongodb &
-sleep 1
-mongo --port 27017  --authenticationDatabase admin -u "admin" -p "changeme"
